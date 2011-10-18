d8caccf9822e7fd20e3ece81d6989cde39b1d1ab
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2011-10-18 19:27:23 -0400

    Completely redid the update checker script

--- What_can_I_update?.sh
+++ What_can_I_update?.sh
@@ -15,13 +15,19 @@ BOLDGREEN="${BOLD}$(tput setaf 2)"
 BOLDWHITE="${BOLD}$(tput setaf 7)"
 BOLDPURPLE="${BOLD}$(tput setaf 5)"
 BOLDRED="${BOLD}$(tput setaf 1)"
+BOLDYELLOW="${BOLD}$(tput setaf 3)"
 
 NEWPKGMSG="${BOLDGREEN}==> ${BOLDWHITE}The following packages can be updated:${RESET}"
+UPTODATEMSG="${BOLDGREEN}==> ${BOLDWHITE}Everything is up to date!${RESET}"
+NOTINSTMSG="${BOLDRED}==> ${BOLDWHITE}The following packages are not installed:${RESET}"
 
 PKGLIST=$(cat README | grep "^[[:digit:]][[:digit:]]:" | sed -e 's/^..:\ \(.*[a-z]\)\ .*->.*$/\1/g')
-NEWPKG=""
+ARRAY_PKGNAME=""
+ARRAY_PKGVER_PKG=""
+ARRAY_PKGVER_INST=""
+ARRAY_COUNTER=0
+NOT_INSTALLED=";"
 
-echo -e "\r$(tput el)${NEWPKGMSG}"
 for i in ${PKGLIST}; do
   pushd "${i}" > /dev/null
 
@@ -29,25 +35,23 @@ for i in ${PKGLIST}; do
   source PKGBUILD
 
   for j in ${pkgname[@]}; do
-    echo -en "\r$(tput el)Comparing package versions of: ${j}"
+    echo -en "\r$(tput el)${BOLD}${BOLDYELLOW}==> ${BOLDWHITE}Comparing package versions of: ${BOLDPURPLE}${j}${RESET}"
 
     # Current installed version
-    CURRENTVER=$(pacman -Qi ${j} | grep [[:digit:]]-[[:digit:]] | sed 's/^.*:\ \(.*\)$/\1/g')
+    CURRENTVER=$(pacman -Qi ${j} 2>/dev/null | grep [[:digit:]]-[[:digit:]] | sed 's/^.*:\ \(.*\)$/\1/g')
 
     # Which version is higher?
-    HIGHERVER=$(echo -e "${CURRENTVER}\n${pkgver}" | sort | tail -n 1)
+    HIGHERVER=$(echo -e "${CURRENTVER}\n${pkgver}-${pkgrel}" | sort | tail -n 1)
 
-    if [[ "${pkgver}" == "${HIGHERVER}" ]]; then
-      SPACES=""
-      SPACES_COLUMN="65"
-      if [ -z "${CURRENTVER}" ]; then CURRENTVER="(not installed)"; fi
-      SPACESCOUNT=$((${SPACES_COLUMN}-6-${#j}-${#pkgver}))
-      while [ "${SPACESCOUNT}" -gt 0 ]; do
-        SPACES="${SPACES} "
-        let SPACESCOUNT--
-      done
-      echo -e "\r$(tput el)${BOLDPURPLE}unity/${BOLDWHITE}${j}${SPACES}${BOLDGREEN}${CURRENTVER}${RESET} -> ${BOLDRED}${pkgver}-${pkgrel}${RESET}"
-      NEWPKG="${NEWPKG}${j}"
+    if [[ "${pkgver}-${pkgrel}" != "${CURRENTVER}" ]] && [[ "${pkgver}-${pkgrel}" == "${HIGHERVER}" ]]; then
+      ARRAY_PKGNAME[${ARRAY_COUNTER}]=${j}
+      ARRAY_PKGVER_PKG[${ARRAY_COUNTER}]=${HIGHERVER}
+      if [ -z "${CURRENTVER}" ]; then
+        ARRAY_PKGVER_INST[${ARRAY_COUNTER}]="notinstalled"
+      else
+        ARRAY_PKGVER_INST[${ARRAY_COUNTER}]=${CURRENTVER}
+      fi
+      let ARRAY_COUNTER++
     fi
   done
 
@@ -57,7 +61,97 @@ for i in ${PKGLIST}; do
   unset pkgname pkgver pkgrel
 done
 
-echo -e "\r$(tput el)"
-if [ -z "${NEWPKG}" ]; then
-  echo "${BOLDGREEN}==> ${BOLDWHITE}Nothing to upgrade :)${RESET}"
+ARRAY_COUNTER=0
+LONGEST_PKGNAME=0
+LONGEST_PKGVER_INST=0
+
+while [ "${ARRAY_COUNTER}" != "$((${#ARRAY_PKGNAME[@]}-1))" ]; do
+  # Do not installed packages that aren't installed in the list
+  if [[ "${ARRAY_PKGVER_INST[${ARRAY_COUNTER}]}" == "notinstalled" ]]; then
+    NOT_INSTALLED="${NOT_INSTALLED}${ARRAY_COUNTER};"
+    let ARRAY_COUNTER++
+    continue
+  fi
+
+  if [[ "${#ARRAY_PKGNAME[${ARRAY_COUNTER}]}" -gt ${LONGEST_PKGNAME} ]]; then
+    LONGEST_PKGNAME=${#ARRAY_PKGNAME[${ARRAY_COUNTER}]}
+  fi
+  if [[ "${#ARRAY_PKGVER_INST[${ARRAY_COUNTER}]}" -gt ${LONGEST_PKGVER_INST} ]]; then
+    LONGEST_PKGVER_INST=${#ARRAY_PKGVER_INST[${ARRAY_COUNTER}]}
+  fi
+  let ARRAY_COUNTER++
+done
+
+if [ "${ARRAY_COUNTER}" -eq 0 ]; then
+  echo -e "\r$(tput el)${UPTODATEMSG}"
+  exit 0
+else
+  echo -e "\r$(tput el)${NEWPKGMSG}"
+fi
+
+ARRAY_COUNTER=0
+
+COLUMN_OF_PKGVER_INST=0
+  let "COLUMN_OF_PKGVER_INST += 6" # for "unity/"
+  let "COLUMN_OF_PKGVER_INST += ${LONGEST_PKGNAME}" # length of the longest package name
+  let "COLUMN_OF_PKGVER_INST += 1" # separator
+COLUMN_OF_PKGVER_PKG=0
+  let "COLUMN_OF_PKGVER_PKG += ${COLUMN_OF_PKGVER_INST}"
+  let "COLUMN_OF_PKGVER_PKG += ${LONGEST_PKGVER_INST}" # length of the longest pkg ver
+  let "COLUMN_OF_PKGVER_PKG += 1" # separator
+
+while [ "${ARRAY_COUNTER}" != "$((${#ARRAY_PKGNAME[@]}-1))" ]; do
+  if $(echo "${NOT_INSTALLED}" | tr ';' '\n' | grep "^${ARRAY_COUNTER}$" 2>/dev/null >/dev/null); then
+    let ARRAY_COUNTER++
+    continue
+  fi
+
+  echo -en "${BOLDPURPLE}unity/"
+  echo -en "${BOLDWHITE}${ARRAY_PKGNAME[${ARRAY_COUNTER}]}"
+  # Column of the package version - the package name length - the length of "unity/"
+  SPACES=$((${COLUMN_OF_PKGVER_INST}-${#ARRAY_PKGNAME[${ARRAY_COUNTER}]}-6))
+  while [ "${SPACES}" -gt 0 ]; do
+    echo -n " "
+    let SPACES--
+  done
+  echo -en "${BOLDGREEN}${ARRAY_PKGVER_INST[${ARRAY_COUNTER}]}"
+  # Column of the new package version - the column of the installed package version
+  #   - the installed package version length
+  SPACES=$((${COLUMN_OF_PKGVER_PKG}-${COLUMN_OF_PKGVER_INST}-${#ARRAY_PKGVER_INST[${ARRAY_COUNTER}]}))
+  while [ "${SPACES}" -gt 0 ]; do
+    echo -n " "
+    let SPACES--
+  done
+  echo -en "${RESET}-> "
+  echo -en "${BOLDRED}${ARRAY_PKGVER_PKG[${ARRAY_COUNTER}]}"
+  echo -e "${RESET}"
+  let ARRAY_COUNTER++
+done
+
+LONGEST_PKGNAME=0
+
+IFS=';' && for i in ${NOT_INSTALLED}; do
+  if [[ "${#ARRAY_PKGNAME[${i}]}" -gt ${LONGEST_PKGNAME} ]]; then
+    LONGEST_PKGNAME=${#ARRAY_PKGNAME[${i}]}
+  fi
+done
+
+COLUMN_NOT_INST_VER=0
+  let "COLUMN_NOT_INST_VER += 6"
+  let "COLUMN_NOT_INST_VER += ${LONGEST_PKGNAME}"
+  let "COLUMN_NOT_INST_VER += 1"
+
+if [[ "${NOT_INSTALLED}" != ";" ]]; then
+  echo -e "\n${NOTINSTMSG}"
+  IFS=';' && for i in ${NOT_INSTALLED}; do
+    echo -en "${BOLDPURPLE}unity/"
+    echo -en "${BOLDWHITE}${ARRAY_PKGNAME[${i}]}"
+    SPACES=$((${COLUMN_NOT_INST_VER}-${#ARRAY_PKGNAME[${i}]}-6))
+    while [ "${SPACES}" -gt 0 ]; do
+      echo -n " "
+      let SPACES--
+    done
+    echo -en "${BOLDYELLOW}(${ARRAY_PKGVER_PKG[${i}]})"
+    echo -e "${RESET}"
+  done
 fi
