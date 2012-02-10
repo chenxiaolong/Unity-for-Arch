f161661a46ae98a7a6b56044a1ebca480bc499c7
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-02-10 01:09:05 -0500

    Add version comparison script for glib2-ubuntu

---
+++ glib2-ubuntu/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/glib2.0' -O - | sed -n 's/.*>glib2.0_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/glib/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
