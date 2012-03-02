eb98d1e87e3c649799e5f29a697d665d53ee8240
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-03-02 08:38:25 -0500

    Add version comparison script for sni-qt

---
+++ sni-qt/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/sni-qt' -O - | sed -n 's/.*>sni-qt_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/sni-qt/+download' -O - | sed -n 's/.*sni-qt-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
