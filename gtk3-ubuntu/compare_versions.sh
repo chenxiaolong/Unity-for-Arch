51f8971ad4f2927910a179811aeb89df7be71c7a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-03-02 06:30:09 -0500

    Add version comparison script for gtk3-ubuntu

---
+++ gtk3-ubuntu/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/gtk+3.0' -O - | sed -n 's/.*>gtk+3.0_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gtk+/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
