eabae378580f2a2585f0f380631c90319e5b3835
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-03-02 09:28:56 -0500

    Add version comparison script for gnome-activity-journal

---
+++ gnome-activity-journal/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/gnome-activity-journal' -O - | sed -n 's/.*>gnome-activity-journal_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/gnome-activity-journal/+download' -O - | sed -n 's/.*gnome-activity-journal-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
