66657d51584845be9a207958c6730c3f3ad1c960
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-03-02 09:20:04 -0500

    Add version comparison script for python2-xapian

---
+++ python2-xapian/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/xapian-bindings' -O - | sed -n 's/.*>xapian-bindings_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'http://oligarchy.co.uk/xapian/' -O - | sed -n 's/.*>\([\.0-9]*\)\/<.*/\1/p' | tail -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
