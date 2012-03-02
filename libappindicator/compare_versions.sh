4c87ce2445493713b7ded6135d11236180daf570
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-03-02 06:51:42 -0500

    Add version comparison script for libappindicator

---
+++ libappindicator/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libappindicator' -O - | sed -n 's/.*>libappindicator_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/libappindicator/+download' -O - | sed -n 's/.*libappindicator-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
