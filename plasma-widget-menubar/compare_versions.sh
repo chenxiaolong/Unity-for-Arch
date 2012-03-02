d76369bbfdc7f3d65d985d33c07cf63c8576c2c5
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-03-02 18:06:21 -0500

    Add version comparison script for plasma-widget-menubar

---
+++ plasma-widget-menubar/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/plasma-widget-menubar' -O - | sed -n 's/.*>plasma-widget-menubar_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/plasma-widget-menubar/+download' -O - | sed -n 's/.*plasma-widget-menubar-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
