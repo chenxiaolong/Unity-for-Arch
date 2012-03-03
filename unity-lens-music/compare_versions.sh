ab33cdf97acf347db2627f7bd6035d577d0113b5
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-03-03 08:22:59 -0500

    Add version comparison script for unity-lens-music

---
+++ unity-lens-music/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/unity-lens-music' -O - | sed -n 's/.*>unity-lens-music_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-lens-music/+download' -O - | sed -n 's/.*unity-lens-music-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
