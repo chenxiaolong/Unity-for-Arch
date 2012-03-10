4fc24cacd37e9d31d0b39eded07b437c640e5481
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-03-09 19:06:00 -0500

    Add version comparison script for qt-ubuntu

---
+++ qt-ubuntu/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/qt4-x11' -O - | sed -n 's/.*>qt4-x11_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q "http://get.qt.nokia.com/qt/source/" -O - | sed -n 's/.*>qt-everywhere-opensource-src-\(.*\)\.tar\.gz<.*/\1/p' | tail -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
