ce87dc871694709fa75d67871e8653c84dc62796
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-03-24 19:26:47 -0400

    [gsettings-desktop-schemas] Fix version comparison script

--- gsettings-desktop-schemas/compare_versions.sh
+++ gsettings-desktop-schemas/compare_versions.sh
@@ -2,14 +2,14 @@
 
 source "$(dirname ${0})/PKGBUILD"
 
-#echo "Getting latest Ubuntu version..."
-#UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/gtk+2.0' -O - | sed -n 's/.*>gtk+2.0_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/gsettings-desktop-schemas' -O - | sed -n 's/.*>gsettings-desktop-schemas_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/gnome/sources/gsettings-desktop-schemas/${pkgver%.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
+UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/gnome/sources/gsettings-desktop-schemas/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
 
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver}"
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
 echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   (n/a)"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
