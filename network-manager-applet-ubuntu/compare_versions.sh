75516751e8063b0447eb3b67782786a6852baf4b
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-04-03 20:32:31 -0400

    [network-manager-applet-ubuntu] Update to version 0.9.4.1 with Ubuntu release 0ubuntu2

--- network-manager-applet-ubuntu/compare_versions.sh
+++ network-manager-applet-ubuntu/compare_versions.sh
@@ -7,10 +7,10 @@ UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/network-manager
 
 echo "Getting latest upstream version..."
 # gotta love Ubuntu versioning :D
-UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/network-manager-applet/${pkgver%.*.*.*.*.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
+UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/network-manager-applet/${pkgver%.*.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
 
 echo ""
 
-echo -e "PKGBUILD version: ${_actual_ver}${_ubuntu_ver} ${_ubuntu_rel}"
+echo -e "PKGBUILD version: ${_actual_ver} ${_ubuntu_rel}"
 echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
