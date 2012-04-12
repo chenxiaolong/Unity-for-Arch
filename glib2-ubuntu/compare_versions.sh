8f7d066c76e5c80fc447193a5e968cf8cc71e95f
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-04-12 17:22:43 -0400

    [glib2-ubuntu] Add Arch Linux version to version comparison script

--- glib2-ubuntu/compare_versions.sh
+++ glib2-ubuntu/compare_versions.sh
@@ -8,8 +8,12 @@ UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/glib2.0' -O - |
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/glib/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
 
+echo "Getting latest Arch Linux version..."
+ARCHLINUX_VER=($(wget -q 'https://www.archlinux.org/packages/core/x86_64/glib2/' -O - | sed -n '/<title>/ s/^.*glib2\ \(.*\)-\(.*\)\ (.*$/\1 \2/p'))
+
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "PKGBUILD version:   ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "Upstream version:   ${UPSTREAM_VER}"
+echo -e "Arch Linux version: ${ARCHLINUX_VER[@]}"
+echo -e "Ubuntu version:     ${UBUNTU_VER[@]}"
