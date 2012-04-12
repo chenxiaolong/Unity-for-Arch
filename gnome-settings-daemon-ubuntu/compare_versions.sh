9bf2b191931013462b03d72f55b220bd425b861b
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-04-12 18:06:53 -0400

    [gsettings-desktop-schemas] Add Arch Linux version to version checker script

--- gnome-settings-daemon-ubuntu/compare_versions.sh
+++ gnome-settings-daemon-ubuntu/compare_versions.sh
@@ -5,11 +5,15 @@ source "$(dirname ${0})/PKGBUILD"
 echo "Getting latest Ubuntu version..."
 UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/gnome-settings-daemon' -O - | sed -n 's/.*>gnome-settings-daemon_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
+echo "Getting latest Arch Linux version..."
+ARCHLINUX_VER=($(wget -q 'https://www.archlinux.org/packages/extra/any/gsettings-desktop-schemas/' -O - | sed -n '/<title>/ s/^.*gsettings-desktop-schemas\ \(.*\)-\(.*\)\ (.*$/\1 \2/p'))
+
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gnome-settings-daemon/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
 
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "PKGBUILD version:   ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "Upstream version:   ${UPSTREAM_VER}"
+echo -e "Arch Linux version: ${ARCHLINUX_VER[@]}"
+echo -e "Ubuntu version:     ${UBUNTU_VER[@]}"
