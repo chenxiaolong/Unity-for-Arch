151451c04c1ab0dc2c50fe85edc379a1b030d97d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-04-12 17:34:28 -0400

    [gtk3-ubuntu] Add Arch Linux version to version comparison script

--- gtk3-ubuntu/compare_versions.sh
+++ gtk3-ubuntu/compare_versions.sh
@@ -5,11 +5,15 @@ source "$(dirname ${0})/PKGBUILD"
 echo "Getting latest Ubuntu version..."
 UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/gtk+3.0' -O - | sed -n 's/.*>gtk+3.0_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
+echo "Getting latest Arch Linux version..."
+ARCHLINUX_VER=($(wget -q 'https://www.archlinux.org/packages/extra/x86_64/gtk3/' -O - | sed -n '/<title>/ s/^.*gtk3\ \(.*\)-\(.*\)\ (.*$/\1 \2/p'))
+
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gtk+/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
 
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "PKGBUILD version:   ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "Upstream version:   ${UPSTREAM_VER}"
+echo -e "Arch Linux version: ${ARCHLINUX_VER[@]}"
+echo -e "Ubuntu version:     ${UBUNTU_VER[@]}"
