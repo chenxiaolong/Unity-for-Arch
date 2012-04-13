ecd62389309bfe94827bf519c4d74b9ca3b9b075
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-04-12 20:51:12 -0400

    [nautilus-ubuntu] Add Arch Linux version to version comparison script

--- nautilus-ubuntu/compare_versions.sh
+++ nautilus-ubuntu/compare_versions.sh
@@ -5,11 +5,15 @@ source "$(dirname ${0})/PKGBUILD"
 echo "Getting latest Ubuntu version..."
 UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/nautilus' -O - | sed -n 's/.*>nautilus_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
+echo "Getting latest Arch Linux version..."
+ARCHLINUX_VER=($(wget -q 'https://www.archlinux.org/packages/extra/x86_64/nautilus/' -O - | sed -n '/<title>/ s/^.*nautilus\ \(.*\)-\(.*\)\ (.*$/\1 \2/p'))
+
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/nautilus/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
 
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "PKGBUILD version:   ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "Upstream version:   ${UPSTREAM_VER}"
+echo -e "Arch Linux version: ${ARCHLINUX_VER[@]}"
+echo -e "Ubuntu version:     ${UBUNTU_VER[@]}"
