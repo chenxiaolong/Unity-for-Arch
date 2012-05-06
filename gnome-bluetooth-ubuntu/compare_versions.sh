cf8c7d2803fc9651c5065499fb5075f3d3124e0a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-05-06 14:08:51 -0400

    [gnome-bluetooth-ubuntu] Fix version comparison script

--- gnome-bluetooth-ubuntu/compare_versions.sh
+++ gnome-bluetooth-ubuntu/compare_versions.sh
@@ -9,11 +9,11 @@ echo "Getting latest Arch Linux version..."
 ARCHLINUX_VER=($(wget -q 'https://www.archlinux.org/packages/extra/x86_64/gnome-bluetooth/' -O - | sed -n '/<title>/ s/^.*gnome-bluetooth\ \(.*\)-\(.*\)\ (.*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gnome-bluetooth/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
+UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gnome-bluetooth/${_actual_ver%.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
 
 echo ""
 
-echo -e "PKGBUILD version:   ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "PKGBUILD version:   ${_actual_ver} Ubuntu ${_ubuntu_ver} ${_ubuntu_rel}"
 echo -e "Upstream version:   ${UPSTREAM_VER}"
 echo -e "Arch Linux version: ${ARCHLINUX_VER[@]}"
 echo -e "Ubuntu version:     ${UBUNTU_VER[@]}"
