38050a93f5db8c3cdbd732321118a41a5894718a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-05-21 11:49:54 -0400

    [libzeitgeist] Update to version 0.3.18 with Ubuntu release 1ubuntu1

--- libzeitgeist/compare_versions.sh
+++ libzeitgeist/compare_versions.sh
@@ -3,13 +3,13 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libzeitgeist' -O - | sed -n 's/.*>libzeitgeist_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/libzeitgeist' -O - | sed -n 's/.*>libzeitgeist_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/libzeitgeist/+download' -O - | sed -n 's/.*libzeitgeist-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
 
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver}"
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
 echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
