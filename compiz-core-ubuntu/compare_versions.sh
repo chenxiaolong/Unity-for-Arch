597620c2273e0c0fbe25248e81a8a30f0246c7dc
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-05-21 12:17:21 -0400

    [compiz-core-ubuntu] Update to version 0.9.7.8 with Ubuntu release 0ubuntu1

--- compiz-core-ubuntu/compare_versions.sh
+++ compiz-core-ubuntu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/compiz' -O - | sed -n 's/.*>compiz_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/compiz' -O - | sed -n 's/.*>compiz_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 #echo "Getting latest upstream version..."
 #UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gtk+/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
