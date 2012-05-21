7ccfcae58b4f8cb64f2c908589e0bf2745af441d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-05-21 12:10:46 -0400

    [unity-lens-music] Switch version comparison script to track precise-updates repo

--- unity-lens-music/compare_versions.sh
+++ unity-lens-music/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/unity-lens-music' -O - | sed -n 's/.*>unity-lens-music_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/unity-lens-music' -O - | sed -n 's/.*>unity-lens-music_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-lens-music/+download' -O - | sed -n 's/.*unity-lens-music-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
