4cb529cce3e72661feb35722b1effe6a551c93ef
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-05-21 12:21:42 -0400

    [unity-core] Switch version comparison script to track precise-updates repo

--- unity-core/compare_versions.sh
+++ unity-core/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/unity' -O - | sed -n 's/.*>unity_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/unity' -O - | sed -n 's/.*>unity_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 # Please name the tarballs appropriately... previously is was all
