1c0c05ee5d585a9c59ee4a06f5babe205e1ed924
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-05-21 12:05:27 -0400

    [nux] Switch version comparison script to track precise-updates repo

--- nux/compare_versions.sh
+++ nux/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/nux' -O - | sed -n 's/.*>nux_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/nux' -O - | sed -n 's/.*>nux_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/nux/+download' -O - | sed -n 's/.*nux-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
