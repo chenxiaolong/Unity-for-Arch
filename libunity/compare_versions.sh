5c668f1aea85b6cefe5d3c2e7b530e2652cd02bc
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-05-21 12:08:19 -0400

    [libunity] Switch version comparison script to track precise-updates repo

--- libunity/compare_versions.sh
+++ libunity/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libunity' -O - | sed -n 's/.*>libunity_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/libunity' -O - | sed -n 's/.*>libunity_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/libunity/+download' -O - | sed -n 's/.*libunity-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
