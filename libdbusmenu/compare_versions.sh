b2d75d5e6d8e24d08e86059b4c51a1fec56ecb7c
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-07-13 20:38:25 -0400

    [libdbusmenu] Update to version 0.6.2 with Ubuntu release 0ubuntu0.1

--- libdbusmenu/compare_versions.sh
+++ libdbusmenu/compare_versions.sh
@@ -3,13 +3,13 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libdbusmenu' -O - | sed -n 's/.*>libdbusmenu_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/libdbusmenu' -O - | sed -n 's/.*>libdbusmenu_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/dbusmenu/+download' -O - | sed -n 's/.*libdbusmenu-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
 
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "PKGBUILD version: ${pkgver%.*.*} ${_ubuntu_rel}"
 echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
