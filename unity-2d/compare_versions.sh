85d406aa137b94a4ebb175da6cddb8cd1f814984
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-07-14 12:00:34 -0400

    [unity-2d] Update to version 5.12.0 with Ubuntu release 0ubuntu1.1

--- unity-2d/compare_versions.sh
+++ unity-2d/compare_versions.sh
@@ -10,7 +10,7 @@ UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/unity-2
 
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "PKGBUILD version: ${pkgver%.*.*} ${_ubuntu_rel}"
 #echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Upstream version: (none)"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
