556d5aeb6a39e5e05d9ed83d24244b9762a164af
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-07-13 20:48:09 -0400

    [bamf] Update to version 0.2.120

--- bamf/compare_versions.sh
+++ bamf/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/bamf' -O - | sed -n 's/.*>bamf_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/bamf' -O - | sed -n 's/.*>bamf_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/bamf/+download' -O - | sed -n 's/.*bamf-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
