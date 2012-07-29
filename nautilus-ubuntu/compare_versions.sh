f5f00790920d7b632b8d3e2e5674a132c18699c8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-07-29 16:43:35 -0400

    [nautilus-ubuntu] Fix version comparison script

--- nautilus-ubuntu/compare_versions.sh
+++ nautilus-ubuntu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/nautilus' -O - | sed -n 's/.*>nautilus_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/nautilus' -O - | sed -n 's/.*>nautilus_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo "Getting latest Arch Linux version..."
 ARCHLINUX_VER=($(wget -q 'https://www.archlinux.org/packages/extra/x86_64/nautilus/' -O - | sed -n '/<title>/ s/^.*nautilus\ \(.*\)-\(.*\)\ (.*$/\1 \2/p'))
