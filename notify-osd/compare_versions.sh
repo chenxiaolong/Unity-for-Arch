dede74387116d76fd2084b47633ce9a7ca5058aa
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-07 21:29:19 -0400

    [notify-osd] Added

---
+++ notify-osd/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/notify-osd' -O - | sed -n 's/.*>notify-osd_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/notify-osd/+download' -O - | sed -n 's/.*notify-osd-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
