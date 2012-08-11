7d6988c683653e9103d3c79c1066d704f78ed7af
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-11 19:11:35 -0400

    [xfce4-indicator-plugin] Update version comparison script

--- xfce4-indicator-plugin/compare_versions.sh
+++ xfce4-indicator-plugin/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/xfce4-indicator-plugin' -O - | sed -n 's/.*>xfce4-indicator-plugin_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/xfce4-indicator-plugin' -O - | sed -n 's/.*>xfce4-indicator-plugin_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "http://goodies.xfce.org/projects/panel-plugins/xfce4-indicator-plugin" -O - | sed -n 's/.*xfce4-indicator-plugin-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
