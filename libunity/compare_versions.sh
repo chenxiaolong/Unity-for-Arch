52bd733e34becf94aeda18e6bf29b8181e713f9d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-14 16:02:06 -0400

    [libunity] Update to version 5.94.0 with Ubuntu release 0ubuntu1

--- libunity/compare_versions.sh
+++ libunity/compare_versions.sh
@@ -3,13 +3,13 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/libunity' -O - | sed -n 's/.*>libunity_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libunity' -O - | sed -n 's/.*>libunity_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/libunity/+download' -O - | sed -n 's/.*libunity-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
 
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver%.*.*} ${_ubuntu_rel}"
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
 echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
