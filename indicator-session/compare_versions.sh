da2bf5aa835872c2d077ee7877d7cb9af3bf4c37
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-14 15:34:33 -0400

    [indicator-session] Update to version 12.10.0

--- indicator-session/compare_versions.sh
+++ indicator-session/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/indicator-session' -O - | sed -n 's/.*>indicator-session_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/indicator-session' -O - | sed -n 's/.*>indicator-session_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-session/+download' -O - | sed -n 's/.*indicator-session-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
