77e4cef68f36071e18ff84ecc15d935bbaa3e0b1
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-14 15:40:37 -0400

    [indicator-sound] Update to version 12.10.0 with Ubuntu release 0ubuntu1

--- indicator-sound/compare_versions.sh
+++ indicator-sound/compare_versions.sh
@@ -3,13 +3,13 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/indicator-sound' -O - | sed -n 's/.*>indicator-sound_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/indicator-sound' -O - | sed -n 's/.*>indicator-sound_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-sound/+download' -O - | sed -n 's/.*indicator-sound-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
 
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver%.*.*} ${_ubuntu_rel}"
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
 echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
