b511e292a83fe997e274ba9bd2a3fb0e3222e344
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-14 18:53:07 -0400

    [unity-lens-files] Update to version 6.2.0

--- unity-lens-files/compare_versions.sh
+++ unity-lens-files/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/unity-lens-files' -O - | sed -n 's/.*>unity-lens-files_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/unity-lens-files' -O - | sed -n 's/.*>unity-lens-files_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-lens-files/+download' -O - | sed -n 's/.*unity-lens-files-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
