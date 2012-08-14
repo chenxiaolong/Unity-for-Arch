f02911590467f7566c652a96026fc9d6fe671945
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-14 15:04:12 -0400

    [libappindicator] Update to version 12.10.0 with Ubuntu release 0ubuntu1

--- libappindicator/compare_versions.sh
+++ libappindicator/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libappindicator' -O - | sed -n 's/.*>libappindicator_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libappindicator' -O - | sed -n 's/.*>libappindicator_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/libappindicator/+download' -O - | sed -n 's/.*libappindicator-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
