dc18ad590ad6255cd081c6a4a4f15e034bf07328
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-14 15:14:16 -0400

    [indicator-appmenu] Update to version 12.10.0

--- indicator-appmenu/compare_versions.sh
+++ indicator-appmenu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/indicator-appmenu' -O - | sed -n 's/.*>indicator-appmenu_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/indicator-appmenu' -O - | sed -n 's/.*>indicator-appmenu_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-appmenu/+download' -O - | sed -n 's/.*indicator-appmenu-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
