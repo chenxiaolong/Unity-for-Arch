6d338a121ef03ffdf031989a3eb4736183420304
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-19 14:41:20 -0400

    [unity-2d] Update to version 5.12.0 with Ubuntu release 0ubuntu3

--- unity-2d/compare_versions.sh
+++ unity-2d/compare_versions.sh
@@ -2,15 +2,8 @@
 
 source "$(dirname ${0})/PKGBUILD"
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/unity-2d' -O - | sed -n 's/.*>unity-2d_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
-
-#echo "Getting latest upstream version..."
-#UPSTREAM_VER=$(wget 'https://launchpad.net/unity-2d/+download' -q -O - | sed -n 's/.*unity[-_]\([\.0-9]*[0-9]\).*\.tar\.bz2.*/\1/p' | head -n 1)
-
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver%.*.*} ${_ubuntu_rel}"
-#echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
 echo -e "Upstream version: (none)"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Ubuntu version:   (none)"
