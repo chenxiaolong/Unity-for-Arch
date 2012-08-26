46372d300a104a2d42b3d42ed2cc6fd49979b2a7
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-26 17:48:58 -0400

    [appmenu-gtk] Update to version 12.10.0

--- appmenu-gtk/compare_versions.sh
+++ appmenu-gtk/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/appmenu-gtk' -O - | sed -n 's/.*>appmenu-gtk_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/appmenu-gtk' -O - | sed -n 's/.*>appmenu-gtk_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/appmenu-gtk/+download' -O - | sed -n 's/.*appmenu-gtk-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
