2f8ed0fcf0fc0e7ccb97052437c08be54ba01499
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-26 18:43:46 -0400

    [overlay-scrollbar] Update to version 0.2.16 with bzr 353 and Ubuntu release 0ubuntu2

--- overlay-scrollbar/compare_versions.sh
+++ overlay-scrollbar/compare_versions.sh
@@ -3,13 +3,13 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/overlay-scrollbar' -O - | sed -n 's/.*>overlay-scrollbar_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/overlay-scrollbar' -O - | sed -n 's/.*>overlay-scrollbar_\(.*\)-\(.*\)\.tar\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/ayatana-scrollbar/+download' -O - | sed -n 's/.*overlay-scrollbar-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
 
 echo ""
 
-echo -e "PKGBUILD version: ${pkgver}"
+echo -e "PKGBUILD version: ${_actual_ver}+${_bzr_rev} ${_ubuntu_rel}"
 echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
