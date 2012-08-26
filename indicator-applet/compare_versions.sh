eaaee02ff3c78200427093eaa9ccaa0325229713
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-26 18:06:49 -0400

    [indicator-applet] Fix version comparison script

--- indicator-applet/compare_versions.sh
+++ indicator-applet/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/indicator-applet' -O - | sed -n 's/.*>indicator-applet_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/indicator-applet' -O - | sed -n 's/.*>indicator-applet_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-applet/+download' -O - | sed -n 's/.*indicator-applet-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
