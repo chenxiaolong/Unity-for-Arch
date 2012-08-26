9f152293497371c41dcedc14ed6aaca499235bc0
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-26 18:23:10 -0400

    [indicator-application] Fix version comparison script

--- indicator-application/compare_versions.sh
+++ indicator-application/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/indicator-application' -O - | sed -n 's/.*>indicator-application_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/indicator-application' -O - | sed -n 's/.*>indicator-application_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-application/+download' -O - | sed -n 's/.*indicator-application-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
