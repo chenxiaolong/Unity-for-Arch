99181e0fe6e54ae5bf19d5f8e7e76b6fe353af1b
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-26 19:24:36 -0400

    [compiz-ubuntu] Update to version 0.9.8 with BZR revision 3319 and Ubuntu release 0ubuntu2

--- compiz-ubuntu/compare_versions.sh
+++ compiz-ubuntu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/compiz' -O - | sed -n 's/.*>compiz_\(.*\)-\(.*\)\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/compiz' -O - | sed -n 's/.*>compiz_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo ""
 
