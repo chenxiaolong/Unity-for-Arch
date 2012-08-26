67ec5dbb5f533abf2d1361a20687e119362f94f3
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-08-26 19:57:05 -0400

    [unity-2d] Dropped

--- unity-2d/compare_versions.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-
-echo ""
-
-echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
-echo -e "Upstream version: (none)"
-echo -e "Ubuntu version:   (none)"
