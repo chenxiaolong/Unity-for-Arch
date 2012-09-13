5e91956c698e73cd4926c9cc307825626f95f204
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-13 11:08:48 -0400

    [libsignon-glib] Added

---
+++ libsignon-glib/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libsignon-glib' -O - | sed -n 's/.*>libsignon-glib_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q "https://code.google.com/p/accounts-sso/downloads/list" -O - | sed -n 's/.*libsignon-glib-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
