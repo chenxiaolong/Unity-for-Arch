721b93ab9702c633cc689df308ed74b6d7025c19
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-13 10:55:53 -0400

    [signon] Added

---
+++ signon/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/signon' -O - | sed -n 's/.*>signon_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q "https://code.google.com/p/accounts-sso/downloads/list" -O - | sed -n 's/.*signon-\([0-9\.]*\)\.tar\.bz2.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
