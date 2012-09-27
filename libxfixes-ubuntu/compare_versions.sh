0208119acd337b742751cb32cc970e63f1fffe24
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-27 00:18:11 -0400

    [libxfixes-ubuntu] Update to version comparison script

--- libxfixes-ubuntu/compare_versions.sh
+++ libxfixes-ubuntu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libxfixes' -O - | sed -n 's/.*>libxfixes_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libxfixes' -O - | sed -n 's/.*>libxfixes_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q http://xorg.freedesktop.org/releases/individual/lib/ -O - | sed -n 's/.*libXfixes-\(.*\).tar.bz2.*/\1/p' | tail -n 1)
