d534893368a4b353508870d7adc9937ba2c7d826
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-26 23:27:31 -0400

    [dconf-qt] Update version comparison script

--- dconf-qt/compare_versions.sh
+++ dconf-qt/compare_versions.sh
@@ -3,14 +3,10 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/dconf-qt' -O - | sed -n 's/.*>dconf-qt_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
-
-#echo "Getting latest upstream version..."
-#UPSTREAM_VER=$(wget -q 'https://launchpad.net/dconf-qt/+download' -O - | sed -n 's/.*dconf-qt-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/dconf-qt' -O - | sed -n 's/.*>dconf-qt_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo ""
 
 echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
-#echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Upstream version: (none)"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
