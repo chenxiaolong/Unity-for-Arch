f0728bf84afd9db40457ad9eee1ff01123d6a0d2
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-26 22:57:50 -0400

    [libunity-misc] Update version comparison script

--- libunity-misc/compare_versions.sh
+++ libunity-misc/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libunity-misc' -O - | sed -n 's/.*>libunity-misc_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libunity-misc' -O - | sed -n 's/.*>libunity-misc_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/libunity-misc/+download' -O - | sed -n 's/.*libunity-misc-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
