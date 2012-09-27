81a23fe5f9e30e2045163521009d62a476a7dfa8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-26 23:25:19 -0400

    [libqtbamf] Update version comparison script

--- libqtbamf/compare_versions.sh
+++ libqtbamf/compare_versions.sh
@@ -3,14 +3,10 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/libqtbamf' -O - | sed -n 's/.*>libqtbamf_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
-
-#echo "Getting latest upstream version..."
-#UPSTREAM_VER=$(wget -q 'https://launchpad.net/bamf-qt/+download' -O - | sed -n 's/.*bamf-qt-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libqtbamf' -O - | sed -n 's/.*>libqtbamf_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo ""
 
 echo -e "PKGBUILD version: ${pkgver}"
-#echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Upstream version: (none)"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
