9dd2eb7e3d4857e5349c49edd10267b0a8a56e56
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-26 23:27:54 -0400

    [sni-qt] Update version comparison script

--- sni-qt/compare_versions.sh
+++ sni-qt/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/sni-qt' -O - | sed -n 's/.*>sni-qt_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/sni-qt' -O - | sed -n 's/.*>sni-qt_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/sni-qt/+download' -O - | sed -n 's/.*sni-qt-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
