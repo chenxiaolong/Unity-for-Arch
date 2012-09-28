aadb53e3d2acf4486ee7cad3b09969af2457ebd7
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-09-27 21:32:16 -0400

    [unity-scope-gdocs] Added

---
+++ unity-scope-gdocs/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/unity-scope-gdocs' -O - | sed -n 's/.*>unity-scope-gdocs_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-lens-gdocs/+download' -O - | sed -n 's/.*unity-scope-gdocs-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
