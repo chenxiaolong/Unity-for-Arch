8c9e9cdf988befa3e7ea3e237fc944bc72eacd01
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-08 15:53:26 -0400

    [unity-lens-photos] Update to version 0.8

--- unity-lens-photos/compare_versions.sh
+++ unity-lens-photos/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/unity-lens-photos' -O - | sed -n 's/.*>unity-lens-photos_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/unity-lens-photos' -O - | sed -n 's/.*>unity-lens-photos_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-lens-photos/+download' -O - | sed -n 's/.*unity-lens-photos-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
