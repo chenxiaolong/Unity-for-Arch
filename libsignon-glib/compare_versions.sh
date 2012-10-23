2edb3280e7cb7738c9623197c2f9e114eaa21c27
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 19:22:15 -0400

    [libsignon-glib] Update version comparison script

--- libsignon-glib/compare_versions.sh
+++ libsignon-glib/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libsignon-glib' -O - | sed -n 's/.*>libsignon-glib_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libsignon-glib' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "https://code.google.com/p/accounts-sso/downloads/list" -O - | sed -n 's/.*libsignon-glib-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
