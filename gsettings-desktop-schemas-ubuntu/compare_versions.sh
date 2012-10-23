45bc84838b6dd4ad70dc6c9d284a5d2218d51d59
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 19:34:58 -0400

    [gsettings-desktop-schemas-ubuntu] Update to version 3.6.0 with Ubuntu release 0ubuntu3

--- gsettings-desktop-schemas-ubuntu/compare_versions.sh
+++ gsettings-desktop-schemas-ubuntu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/gsettings-desktop-schemas' -O - | sed -n 's/.*>gsettings-desktop-schemas_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/gsettings-desktop-schemas' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/gnome/sources/gsettings-desktop-schemas/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
