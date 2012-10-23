2415f9bca50fb13072fd576f378f0732d5fa1e0e
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 19:21:06 -0400

    [signon] Update version comparison script

--- signon/compare_versions.sh
+++ signon/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/signon' -O - | sed -n 's/.*>signon_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/signon' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "https://code.google.com/p/accounts-sso/downloads/list" -O - | sed -n 's/.*signon-\([0-9\.]*\)\.tar\.bz2.*/\1/p' | head -n 1)
