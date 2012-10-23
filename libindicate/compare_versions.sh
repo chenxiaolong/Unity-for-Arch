5b1b571355783cbd1d355301e9a77fb746fb6237
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 18:33:14 -0400

    [libindicate] Update version comparison script

--- libindicate/compare_versions.sh
+++ libindicate/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libindicate' -O - | sed -n 's/.*>libindicate_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libindicate' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/libindicate/+download' -O - | sed -n 's/.*libindicate-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
