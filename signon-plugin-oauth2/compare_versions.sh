f239f2a482faae11a24266876a0248cc5848812b
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 19:23:37 -0400

    [signon-plugin-oauth2] Update version comparison script

--- signon-plugin-oauth2/compare_versions.sh
+++ signon-plugin-oauth2/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/signon-plugin-oauth2' -O - | sed -n 's/.*>signon-plugin-oauth2_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/signon-plugin-oauth2' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "https://code.google.com/p/accounts-sso/downloads/list" -O - | sed -n 's/.*signon-oauth2-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
