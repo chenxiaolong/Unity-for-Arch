62241d3a4d35305ff41815202d3ef6410dd39352
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 19:18:32 -0400

    [ido] Update to version 12.10.2

--- ido/compare_versions.sh
+++ ido/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/ido' -O - | sed -n 's/.*>ido_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/ido' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/ido/+download' -O - | sed -n 's/.*ido-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
