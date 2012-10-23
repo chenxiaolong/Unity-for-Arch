5fb827d341233b988bf551602d797b154794fc4a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 19:12:54 -0400

    [dconf-qt] Update version comparison script

--- dconf-qt/compare_versions.sh
+++ dconf-qt/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/dconf-qt' -O - | sed -n 's/.*>dconf-qt_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/dconf-qt' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo ""
 
