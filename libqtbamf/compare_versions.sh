7817fd95e808516452cbc7513a341703b2e35146
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 19:11:16 -0400

    [libqtbamf] Update version comparison script

--- libqtbamf/compare_versions.sh
+++ libqtbamf/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libqtbamf' -O - | sed -n 's/.*>libqtbamf_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libqtbamf' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo ""
 
