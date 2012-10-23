2af31019391417b4b06a6e0716398d468b21ed2b
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 19:20:31 -0400

    [libaccounts-qt] Update version comparison script

--- libaccounts-qt/compare_versions.sh
+++ libaccounts-qt/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libaccounts-qt' -O - | sed -n 's/.*>libaccounts-qt_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libaccounts-qt' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q "https://code.google.com/p/accounts-sso/downloads/list" -O - | sed -n 's/.*accounts-qt-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
