7ca7b5734500ec7e5de4a7f824e8f5baf14bcb52
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 19:21:45 -0400

    [signon-ui] Update version comparison script

--- signon-ui/compare_versions.sh
+++ signon-ui/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/signon-ui' -O - | sed -n 's/.*>signon-ui_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/signon-ui' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/online-accounts-signon-ui/+download' -O - | sed -n 's/.*signon-ui-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
