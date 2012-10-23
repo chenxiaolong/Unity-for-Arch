bb8671674d684231ef05ec2982a8aede2fa96e83
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 19:19:10 -0400

    [check] Update version comparison script

--- check/compare_versions.sh
+++ check/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/check' -O - | sed -n 's/.*>check_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/check' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'http://sourceforge.net/projects/check/files/check/' -O - | sed -n 's/.*Download check-\(.*\)\.tar\.gz (.*/\1/p')
