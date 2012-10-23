962f76d870b1889b9f3679b41321fbb7b6178774
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 19:12:23 -0400

    [libqtgconf] Update version comparison script

--- libqtgconf/compare_versions.sh
+++ libqtgconf/compare_versions.sh
@@ -3,14 +3,10 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libqtgconf' -O - | sed -n 's/.*>libqtgconf_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
-
-#echo "Getting latest upstream version..."
-#UPSTREAM_VER=$(wget -q 'https://launchpad.net/gconf-qt/+download' -O - | sed -n 's/.*gconf-qt-\(.*\)\.tar\.bz2.*/\1/p' | head -n 1)
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libqtgconf' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo ""
 
 echo -e "PKGBUILD version: ${pkgver}"
-#echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Upstream version: (none)"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
