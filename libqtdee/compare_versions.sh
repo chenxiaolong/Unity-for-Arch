df96a9afa177c771f45b338da9518e1e535be728
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 19:11:44 -0400

    [libqtdee] Update version comparison script

--- libqtdee/compare_versions.sh
+++ libqtdee/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libqtdee' -O - | sed -n 's/.*>libqtdee_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libqtdee' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo ""
 
