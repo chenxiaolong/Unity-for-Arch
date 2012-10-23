436e5bdd827b389aee3ca7b8d4a46a2b363d3319
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 18:54:41 -0400

    [libunity-webapps] Update to version 2.4.1

--- libunity-webapps/compare_versions.sh
+++ libunity-webapps/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/libunity-webapps' -O - | sed -n 's/.*>libunity-webapps_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libunity-webapps' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/libunity-webapps/+download' -O - | sed -n 's/.*unity_webapps-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
