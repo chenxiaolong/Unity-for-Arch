ab358cbff6036cd3203ee66d8915d46e3752d7e4
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 21:43:31 -0400

    [frame] Update version comparison script

--- frame/compare_versions.sh
+++ frame/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/frame' -O - | sed -n 's/.*>frame_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/frame' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/frame/+download' -O - | sed -n 's/.*frame-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
