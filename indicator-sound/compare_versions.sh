379fe85e3f507b7fe9ffe6617d45c180bf61feeb
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 21:21:24 -0400

    [indicator-sound] Update version comparison script

--- indicator-sound/compare_versions.sh
+++ indicator-sound/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/indicator-sound' -O - | sed -n 's/.*>indicator-sound_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/indicator-sound' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-sound/+download' -O - | sed -n 's/.*indicator-sound-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
