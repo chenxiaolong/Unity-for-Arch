f98b85b2a2504ecb346661c0bfd9b26db0ad2165
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 22:16:56 -0400

    [python2-geoclue] Update version comparison script

--- python2-geoclue/compare_versions.sh
+++ python2-geoclue/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/python-geoclue' -O - | sed -n 's/.*>python-geoclue_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/python-geoclue' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://live.gnome.org/gtg/soc/python_geoclue' -O - | sed -n 's/.*python-geoclue-\(.*\)\.tar\.gz.*/\1/p')
