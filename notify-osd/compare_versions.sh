7e654f4e0293db7e1a162d86306a9971d6fd7097
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 21:30:56 -0400

    [notify-osd] Update to version 0.9.34 with Ubuntu release 0ubuntu5

--- notify-osd/compare_versions.sh
+++ notify-osd/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/notify-osd' -O - | sed -n 's/.*>notify-osd_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/notify-osd' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/notify-osd/+download' -O - | sed -n 's/.*notify-osd-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
