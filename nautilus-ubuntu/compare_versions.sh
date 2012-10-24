dca8be0e317696d231d4655d9e23857f2d095449
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 22:09:08 -0400

    [nautilus-ubuntu] Update version 3.6.1 with GNOME 3 PPA release 0ubuntu1~ubuntu12.10.2

--- nautilus-ubuntu/compare_versions.sh
+++ nautilus-ubuntu/compare_versions.sh
@@ -3,17 +3,21 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise-updates/source/nautilus' -O - | sed -n 's/.*>nautilus_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/nautilus' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest Arch Linux version..."
 ARCHLINUX_VER=($(wget -q 'https://www.archlinux.org/packages/extra/x86_64/nautilus/' -O - | sed -n '/<title>/ s/^.*nautilus\ \(.*\)-\(.*\)\ (.*$/\1 \2/p'))
 
+echo "Getting latest GNOME 3 PPA version..."
+PPA_VER=($(wget -q 'http://ppa.launchpad.net/gnome3-team/gnome3/ubuntu/pool/main/n/nautilus/' -O - | sed -n 's/.*>nautilus_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p' | tail -n 1))
+
 echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/nautilus/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
+UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/nautilus/${_actual_ver%.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
 
 echo ""
 
-echo -e "PKGBUILD version:   ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "PKGBUILD version:   ${_actual_ver} ${_ppa_rel}"
 echo -e "Upstream version:   ${UPSTREAM_VER}"
 echo -e "Arch Linux version: ${ARCHLINUX_VER[@]}"
 echo -e "Ubuntu version:     ${UBUNTU_VER[@]}"
+echo -e "PPA version:        ${PPA_VER[@]}"
