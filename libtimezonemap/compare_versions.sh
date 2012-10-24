231f5e0117ba740fcb3dc310e5dd05a16886e60a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 21:12:25 -0400

    [libtimezonemap] Update version comparison script

--- libtimezonemap/compare_versions.sh
+++ libtimezonemap/compare_versions.sh
@@ -3,14 +3,10 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/libtimezonemap' -O - | sed -n 's/.*>libtimezonemap_\(.*\)\.tar\.gz<.*/\1/p'))
-
-#echo "Getting latest upstream version..."
-#UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-application/+download' -O - | sed -n 's/.*indicator-application-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/libtimezonemap' | sed -n 's/^.*current\ release\ (\(.*\)).*$/\1/p'))
 
 echo ""
 
 echo -e "PKGBUILD version: ${pkgver}"
-#echo -e "Upstream version: ${UPSTREAM_VER}"
 echo -e "Upstream version: (none)"
 echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
