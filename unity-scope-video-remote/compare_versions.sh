da79bc8a74bab50324766caeb5a08a2402a4f9f9
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 22:13:44 -0400

    [unity-scope-video-remote] Update version comparison script

--- unity-scope-video-remote/compare_versions.sh
+++ unity-scope-video-remote/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/unity-scope-video-remote' -O - | sed -n 's/.*>unity-scope-video-remote_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/unity-scope-video-remote' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-lens-videos/+download' -O - | sed -n 's/.*unity-scope-video-remote-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
