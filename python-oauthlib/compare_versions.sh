6297ddf18b7357879ec5b12c5986b11124b3c243
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 22:09:54 -0400

    [python-oauthlib] Update version comparison script

--- python-oauthlib/compare_versions.sh
+++ python-oauthlib/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/python-oauthlib' -O - | sed -n 's/.*>python-oauthlib_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/python-oauthlib' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'http://pypi.python.org/pypi/oauthlib' -O - | sed -n 's/.*>oauthlib-\(.*\)\.tar\.gz<.*/\1/p' | head -1)
