b00616d4b96615e00e16950ab492ff94aa45bdc0
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 22:15:52 -0400

    [unity] Update version comparison script

--- unity/compare_versions.sh
+++ unity/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/unity' -O - | sed -n 's/.*>unity_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/unity' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 # Please name the tarballs appropriately... previously is was all
