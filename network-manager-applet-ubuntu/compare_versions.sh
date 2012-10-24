96d3ae79f91bd413b956c61f3df2e60622b27453
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 21:33:16 -0400

    [network-manager-applet-ubuntu] Update version comparison script

--- network-manager-applet-ubuntu/compare_versions.sh
+++ network-manager-applet-ubuntu/compare_versions.sh
@@ -3,14 +3,13 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/network-manager-applet' -O - | sed -n 's/.*>network-manager-applet_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/network-manager-applet' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest Arch Linux version..."
 ARCHLINUX_VER=($(wget -q 'https://www.archlinux.org/packages/extra/x86_64/network-manager-applet/' -O - | sed -n '/<title>/ s/^.*network-manager-applet\ \(.*\)-\(.*\)\ (.*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
-# gotta love Ubuntu versioning :D
-UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/network-manager-applet/${pkgver%.*.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
+UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/network-manager-applet/${_actual_ver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
 
 echo ""
 
