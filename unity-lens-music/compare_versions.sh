533d04b5dfadc35b623faedef65aa987bbc8eb52
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 22:11:50 -0400

    [unity-lens-music] Update version comparison script

--- unity-lens-music/compare_versions.sh
+++ unity-lens-music/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/unity-lens-music' -O - | sed -n 's/.*>unity-lens-music_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/unity-lens-music' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-lens-music/+download' -O - | sed -n 's/.*unity-lens-music-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
