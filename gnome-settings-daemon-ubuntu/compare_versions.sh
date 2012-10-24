70a8751dfdffcd45c7878468ac11f4c9a9c21580
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 20:04:36 -0400

    [gnome-settings-daemon-ubuntu] Update to version 3.6.1 with Ubuntu version Ubuntu 3.4.2 and Ubuntu release 0ubuntu14

--- gnome-settings-daemon-ubuntu/compare_versions.sh
+++ gnome-settings-daemon-ubuntu/compare_versions.sh
@@ -3,17 +3,17 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/gnome-settings-daemon' -O - | sed -n 's/.*>gnome-settings-daemon_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/gnome-settings-daemon' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest Arch Linux version..."
 ARCHLINUX_VER=($(wget -q 'https://www.archlinux.org/packages/extra/any/gsettings-desktop-schemas/' -O - | sed -n '/<title>/ s/^.*gsettings-desktop-schemas\ \(.*\)-\(.*\)\ (.*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gnome-settings-daemon/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
+UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gnome-settings-daemon/${_actual_ver%.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
 
 echo ""
 
-echo -e "PKGBUILD version:   ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "PKGBUILD version:   ${_actual_ver} Ubuntu ${_ubuntu_ver} ${_ubuntu_rel}"
 echo -e "Upstream version:   ${UPSTREAM_VER}"
 echo -e "Arch Linux version: ${ARCHLINUX_VER[@]}"
 echo -e "Ubuntu version:     ${UBUNTU_VER[@]}"
