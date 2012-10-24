ce88333d418f7e8ce0082b96b93aad7a873dcfaa
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 21:17:05 -0400

    [indicator-power] Update to version 12.10.2

--- indicator-power/compare_versions.sh
+++ indicator-power/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/indicator-power' -O - | sed -n 's/.*>indicator-power_\(.*\)-\(.*\)\.diff\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/indicator-power' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-power/+download' -O - | sed -n 's/.*indicator-power-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
