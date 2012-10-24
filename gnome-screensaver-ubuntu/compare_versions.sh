db15a27d21029d8334517a8aac4a7c12d463c5ad
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 21:06:59 -0400

    [gnome-screensaver-ubuntu] Update to version 3.6.1 with Ubuntu release 3 6.0 and release 0ubuntu1

--- gnome-screensaver-ubuntu/compare_versions.sh
+++ gnome-screensaver-ubuntu/compare_versions.sh
@@ -3,17 +3,17 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/gnome-screensaver' -O - | sed -n 's/.*>gnome-screensaver_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/gnome-screensaver' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest Arch Linux version..."
 ARCHLINUX_VER=($(wget -q 'https://www.archlinux.org/packages/extra/x86_64/gnome-screensaver/' -O - | sed -n '/<title>/ s/^.*gnome-screensaver\ \(.*\)-\(.*\)\ (.*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gnome-screensaver/${pkgver%.*.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
+UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gnome-screensaver/${_actual_ver%.*}/" -O - | sed -n 's/.*>LATEST-IS-\(.*\)<.*/\1/p')
 
 echo ""
 
-echo -e "PKGBUILD version:   ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "PKGBUILD version:   ${_actual_ver} Ubuntu ${_ubuntu_ver} ${_ubuntu_rel}"
 echo -e "Upstream version:   ${UPSTREAM_VER}"
 echo -e "Arch Linux version: ${ARCHLINUX_VER[@]}"
 echo -e "Ubuntu version:     ${UBUNTU_VER[@]}"
