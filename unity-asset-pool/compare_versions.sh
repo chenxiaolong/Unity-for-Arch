f2dbabd1b51589fd10e56241ca53aadd908a62a9
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 21:57:46 -0400

    [unity-asset-pool] Update version comparison script

--- unity-asset-pool/compare_versions.sh
+++ unity-asset-pool/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/quantal/source/unity-asset-pool' -O - | sed -n 's/.*>unity-asset-pool_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/unity-asset-pool' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-asset-pool/+download' -O - | sed -n 's/.*unity-asset-pool-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
