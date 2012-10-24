a57643f2fbf37bb25db7f70633e0c349bfaf6bf4
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-10-23 22:17:56 -0400

    [python2-xapian] Update version comparison script

--- python2-xapian/compare_versions.sh
+++ python2-xapian/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q 'http://packages.ubuntu.com/precise/source/xapian-bindings' -O - | sed -n 's/.*>xapian-bindings_\(.*\)-\(.*\)\.debian\.tar\.gz<.*/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/xapian-bindings' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'http://oligarchy.co.uk/xapian/' -O - | sed -n 's/.*>\([\.0-9]*\)\/<.*/\1/p' | tail -n 1)
