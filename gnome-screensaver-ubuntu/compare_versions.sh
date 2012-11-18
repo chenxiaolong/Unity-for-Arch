1f5d3264c2b1d7c917653df3fb4d72349cec9a63
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-11-17 21:12:59 -0500

    [gnome-screensaver-ubuntu] Update to version 3.6.1 with Ubuntu release 0ubuntu1

--- gnome-screensaver-ubuntu/compare_versions.sh
+++ gnome-screensaver-ubuntu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/gnome-screensaver' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/raring/+source/gnome-screensaver' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest Arch Linux version..."
 ARCHLINUX_VER=($(wget -q 'https://www.archlinux.org/packages/extra/x86_64/gnome-screensaver/' -O - | sed -n '/<title>/ s/^.*gnome-screensaver\ \(.*\)-\(.*\)\ (.*$/\1 \2/p'))
@@ -13,7 +13,7 @@ UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gnome-screensaver
 
 echo ""
 
-echo -e "PKGBUILD version:   ${_actual_ver} Ubuntu ${_ubuntu_ver} ${_ubuntu_rel}"
+echo -e "PKGBUILD version:   ${_actual_ver} ${_ubuntu_rel}"
 echo -e "Upstream version:   ${UPSTREAM_VER}"
 echo -e "Arch Linux version: ${ARCHLINUX_VER[@]}"
 echo -e "Ubuntu version:     ${UBUNTU_VER[@]}"
