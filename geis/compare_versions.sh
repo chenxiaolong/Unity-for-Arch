bcf45b9c7ab682281bb972e4b266ded24a1128a5
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-11-17 21:46:57 -0500

    [geis] Update to version 2.2.14

--- geis/compare_versions.sh
+++ geis/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/geis' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/raring/+source/geis' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/geis/+download' -O - | sed -n 's/.*geis-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
