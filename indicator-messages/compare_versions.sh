ed60977bf5a975ba1b399ae69869367a818f9e27
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-11-17 20:28:38 -0500

    [indicator-messages] Update to version 12.10.5

--- indicator-messages/compare_versions.sh
+++ indicator-messages/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/indicator-messages' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/raring/+source/indicator-messages' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-messages/+download' -O - | sed -n 's/.*indicator-messages-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
