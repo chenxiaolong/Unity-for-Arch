0cfac3a2003a5774bfc343df109c25e7c9eba59d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-11-17 21:45:15 -0500

    [grail] Update to version 3.0.8

--- grail/compare_versions.sh
+++ grail/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/grail' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/raring/+source/grail' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest upstream version..."
 UPSTREAM_VER=$(wget -q 'https://launchpad.net/grail/+download' -O - | sed -n 's/.*grail-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
