52c39b36c048aa39ccce853b07413404c14d7818
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-12-20 01:56:31 -0500

    [gnome-control-center-unity] Added

---
+++ gnome-control-center-unity/compare_versions.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/raring/+source/gnome-control-center-unity' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: (none)"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
