347ec3da29f866b75d6734e9f891940f03c8a454
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-12-20 03:26:48 -0500

    Revert "[indicator-sound] Removed: https://bbs.archlinux.org/viewtopic.php?pid=1199369#p1199369"
    This reverts commit 17971d79ecd689b8feb865b67e27c483c0768653.
    
    Conflicts:
    	README

---
+++ indicator-sound/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/indicator-sound' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-sound/+download' -O - | sed -n 's/.*indicator-sound-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
