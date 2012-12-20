c3e77c53cb8c20bc06561e4951c63a9e6e6c8457
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-12-19 23:11:23 -0500

    Revert "[indicator-application] Removed: https://bbs.archlinux.org/viewtopic.php?pid=1199369#p1199369"
    This reverts commit d9bc9c3ebd801fccb91d7e478bb01615e49ea194.
    
    Conflicts:
    	README

---
+++ indicator-application/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/indicator-application' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/indicator-application/+download' -O - | sed -n 's/.*indicator-application-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
