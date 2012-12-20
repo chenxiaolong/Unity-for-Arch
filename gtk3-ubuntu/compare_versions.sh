ae9f177b86b72be8bbb1c65275b4b8a7f9376764
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2012-12-19 20:05:17 -0500

    [gtk3-ubuntu] Update to version 3.6.2 with Ubuntu release 0ubuntu1

--- gtk3-ubuntu/compare_versions.sh
+++ gtk3-ubuntu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 
 echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/gtk+3.0' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/raring/+source/gtk+3.0' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
 
 echo "Getting latest Arch Linux version..."
 ARCHLINUX_VER=($(wget -q 'https://www.archlinux.org/packages/extra/x86_64/gtk3/' -O - | sed -n '/<title>/ s/^.*gtk3\ \(.*\)-\(.*\)\ (.*$/\1 \2/p'))
@@ -13,7 +13,7 @@ UPSTREAM_VER=$(wget -q "http://ftp.gnome.org/pub/GNOME/sources/gtk+/${_actual_ve
 
 echo ""
 
-echo -e "PKGBUILD version:   ${_actual_ver} Ubuntu ${_ubuntu_ver} ${_ubuntu_rel}"
+echo -e "PKGBUILD version:   ${_actual_ver} ${_ubuntu_rel}"
 echo -e "Upstream version:   ${UPSTREAM_VER}"
 echo -e "Arch Linux version: ${ARCHLINUX_VER[@]}"
 echo -e "Ubuntu version:     ${UBUNTU_VER[@]}"
