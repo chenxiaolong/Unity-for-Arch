eb7e7b6dde4af033e34395bc49d200bc78e7df56
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-07 14:44:18 -0500

    [unity] Changelog:
    * Update to version 6.12.0daily12.12.05 with Ubuntu release 0ubuntu1
    * Do not fail on broken patches (no '-N' for patch)
    * Drop 0001_fix_gtest_directory.patch
      - Replaced with 0001_Remove_gtest.patch
      - Unity's CMakeLists.txt does not work well with Arch's gtest
    * Drop 0002_CMake_workaround
      - Fixed upstream
    * Drop 0003_Ignore_deprecated_schema_path.patch
      - Fixed upstream
    * Drop 0005_link_dbus-glib.patch
      - Fixed upstream
    * Drop 0007_link_gmodule.patch
      - Fixed upstream
    * Drop 0008_Revert_port_to_atk-bridge-2.0.patch
      - Arch has GNOME 3.6 now

--- unity/compare_versions.sh
+++ unity/compare_versions.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/raring/+source/unity' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-# Please name the tarballs appropriately... previously is was all
-# name-version.tar.bz2 and now there's name_version.orig.tar.bz2. Please let the
-# debian source naming go in the repos only, not upstream!
-UPSTREAM_VER=$(wget 'https://launchpad.net/unity/+download' -q -O - | sed -n 's/.*unity[-_]\([\.0-9]*[0-9]\).*\.tar\.\(bz2\|gz\).*/\1/p' | head -n 1)
-
-echo ""
-
-echo -e "PKGBUILD version: ${_actual_ver} ${_ubuntu_rel}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver} ${_ubuntu_rel}"
+echo -e "Upstream version: $(get_launchpad_version ${pkgname})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} raring)"
