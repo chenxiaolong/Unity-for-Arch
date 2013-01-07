8e08a881b4f734907262472a8f73a278299ab867
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-07 07:21:58 -0500

    Revert "[unity-lens-music] Removed: https://bbs.archlinux.org/viewtopic.php?pid=1199369#p1199369"
    This reverts commit ac919427e4eee6bbee37818ce1ca63896f43b7b2.
    
    Conflicts:
    	README

---
+++ unity-lens-music/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/unity-lens-music' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-lens-music/+download' -O - | sed -n 's/.*unity-lens-music-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
