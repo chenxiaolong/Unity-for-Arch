f1e925a1b413b949dabf0c5845ff6598faea8fb7
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-07 07:32:04 -0500

    [python-oauthlib] Use new version comparison script

--- python-oauthlib/compare_versions.sh
+++ python-oauthlib/compare_versions.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
-
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/raring/+source/python-oauthlib' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'http://pypi.python.org/pypi/oauthlib' -O - | sed -n 's/.*>oauthlib-\(.*\)\.tar\.gz<.*/\1/p' | head -1)
-
-echo ""
+source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "Upstream version: $(get_pypi_version oauthlib)"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} raring)"
