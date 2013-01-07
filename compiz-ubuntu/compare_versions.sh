4fc066eb3605599be6466d70da17a412304f8407
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-07 13:28:26 -0500

    [compiz-ubuntu] Update to version 0.9.9~daily12.12.05 with Ubuntu release 0ubuntu2

--- compiz-ubuntu/compare_versions.sh
+++ compiz-ubuntu/compare_versions.sh
@@ -3,6 +3,6 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version: ${_actual_ver}+bzr${_bzr_rev} ${_ubuntu_rel}"
+echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver} ${_ubuntu_rel}"
 echo -e "Upstream version: $(get_launchpad_version compiz)"
 echo -e "Ubuntu version:   $(get_ubuntu_version compiz raring)"
