ef4349b1d59e0176cb9a5df2f39331d032753351
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-07 07:17:07 -0500

    [version_checker.sh] Add script to hold the common code for version checking

---
+++ version_checker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Do not run this manually
+
+get_ubuntu_version() {
+  if [ -z "${1}" ]; then
+    echo "No package was provided"
+    exit 1
+  fi
+  if [ -z "${2}" ]; then
+    echo "No Ubuntu version was provided"
+    exit 1
+  fi
+  wget -q -O - "https://launchpad.net/ubuntu/${2}/+source/${1}" | \
+    sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'
+}
+
+get_launchpad_version() {
+  if [ -z "${1}" ]; then
+    echo "No package was provided"
+    exit 1
+  fi
+  wget -q -O - "https://launchpad.net/${1}/+download" | \
+    sed -n "s/.*${1}-\(.*\)\.tar.*/\1/p" | head -n 1
+}
