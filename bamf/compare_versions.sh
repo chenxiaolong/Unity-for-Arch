56b20cab8677c81154ed15c28906a96552aadde8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-07 14:47:49 -0500

    [bamf] Changelog:
    * Update to version 0.4.0daily12.12.05 with Ubuntu release 0ubuntu2
    * Drop deprecated GTK 2 build

--- bamf/compare_versions.sh
+++ bamf/compare_versions.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/bamf' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/bamf/+download' -O - | sed -n 's/.*bamf-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
-
-echo ""
-
-echo -e "PKGBUILD version: ${pkgver}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver} ${_ubuntu_rel}"
+echo -e "Upstream version: $(get_launchpad_version ${pkgbase})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgbase} raring)"
