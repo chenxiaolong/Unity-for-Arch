87b5a1f84095b211fe12c0a681cb885a1517c18d
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-07 07:05:41 -0500

    Revert "[unity-asset-pool] Removed: https://bbs.archlinux.org/viewtopic.php?pid=1199369#p1199369"
    This reverts commit c59add324fbe788ff68cf8d9453e562c83f71b6b.
    
    Conflicts:
    	README

---
+++ unity-asset-pool/compare_versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+
+echo "Getting latest Ubuntu version..."
+UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/unity-asset-pool' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
+
+echo "Getting latest upstream version..."
+UPSTREAM_VER=$(wget -q 'https://launchpad.net/unity-asset-pool/+download' -O - | sed -n 's/.*unity-asset-pool-\(.*\)\.tar\.gz.*/\1/p' | head -n 1)
+
+echo ""
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
