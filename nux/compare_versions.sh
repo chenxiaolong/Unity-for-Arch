3d21ed4d002c12088176bcc6a6e216194eaf4411
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-07 13:48:19 -0500

    [nux] Update to version 4.0.0daily12.12.05 with Ubuntu release 0ubuntu1

--- nux/compare_versions.sh
+++ nux/compare_versions.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(dirname ${0})/PKGBUILD"
+source "$(dirname ${0})/../version_checker.sh"
 
-echo "Getting latest Ubuntu version..."
-UBUNTU_VER=($(wget -q -O - 'https://launchpad.net/ubuntu/quantal/+source/nux' | sed -n 's/^.*current\ release\ (\(.*\)-\(.*\)).*$/\1 \2/p'))
-
-echo "Getting latest upstream version..."
-UPSTREAM_VER=$(wget -q 'https://launchpad.net/nux/+download' -O - | sed -n 's/.*nux[_-]\(.*\)\.tar\.gz.*/\1/p' | head -n 1 | sed 's/.orig//')
-
-echo ""
-
-echo -e "PKGBUILD version: ${pkgver}"
-echo -e "Upstream version: ${UPSTREAM_VER}"
-echo -e "Ubuntu version:   ${UBUNTU_VER[@]}"
+echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver} ${_ubuntu_rel}"
+echo -e "Upstream version: $(get_launchpad_version ${pkgname})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} raring)"
