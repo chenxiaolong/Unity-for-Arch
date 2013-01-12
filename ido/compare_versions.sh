8f019d3ce14c37931b8dd1e5a7315a6907001a75
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-01-12 10:18:44 -0500

    [ido] Update to version 12.10.3daily13.01.11

--- ido/compare_versions.sh
+++ ido/compare_versions.sh
@@ -3,6 +3,6 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version: ${pkgver}"
+echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver}"
 echo -e "Upstream version: $(get_launchpad_version ${pkgname})"
 echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-raring})"
