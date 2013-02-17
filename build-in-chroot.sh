8d97febeacbf7a2448a97351ebebb6153d83fd2e
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-02-16 19:59:56 -0500

    [build-in-chroot.sh] Changelog:
    * Threading/parallel execution issues should be solved once and for all
      - Each process gets it's own cache directory (copy of
        /var/cache/pacman/pkg/)
      - New structure
        + Split into four distinct stages: download, build, publish. The
          download stage is mutually exclusive as it needs to touch the
          shared cache directory. The publish stage is also mutually
          exclusive for the same reason.
        + Download stage:
          1. Build a mini-chroot containing only database files (x86_64 and
             i686 databases can't both go in /var/lib/pacman/sync/ on the
             host)
          2. Remove any packages in the local repo from the host's pacman
             cache
          3. Download the base packages for the chroot into the host's
             cache
          4. Download the dependencies and build dependencies for the
             target package into the host's cache, skipping any packages
             present in the local repository
          5. Copy the host's pacman cache to a temporary directory for use
             during the build process
        + Build stage:
          1. Create chroot with base packages. All the needed packages
             should have been downloaded already
          2. Set up /etc/makepkg.conf, /etc/pacman.conf, and the sudo
             configuration
          3. Copy packaging into chroot
          4. Create new build user inside chroot and set the appropriate
             permissions
          5. Update pacman databases inside chroot and download remaining
             packages, which should only be those in the local repo and
             those that were updated between this stage and the previous
             stage
          6. Build package
        + Publish stage:
          1. Optionally copy built packages to PKGBUILD directory
          2. Remove old database for local repo
          3. Copy built packages to local repo
          4. Build database for local repo
    * Avoid reading PKGBUILD files as root
    * Removed locks where they were no longer required
    * Make sure the mini-chroot, temporary cache directory and nobody-user-
      readable PKGBUILD are removed on exit

--- build-in-chroot.sh
+++ build-in-chroot.sh
@@ -31,6 +31,7 @@ show_help() {
 ARCH_SUPPORTED=('i686' 'x86_64')
 ARCH=""
 PACKAGE_DIR=""
+CHROOT_PACKAGES=('base' 'base-devel' 'sudo' 'curl')
 
 ARGS=$(getopt -o p:a:k -l package:arch:keep -n build-in-chroot.sh -- "${@}")
 
@@ -125,20 +126,86 @@ cleanup() {
   # Clean up chroot
   rm -rf ${CHROOT}
   rm -f ${CHROOT}.lock
+  rm -rf ${CACHE_DIR}
+  rm -rf ${TEMP_CHROOT}
+  rm -f ${TEMP_PKGBUILD}
 }
 
 trap "cleanup" SIGINT SIGTERM SIGKILL EXIT
 
-CHROOT=$(mktemp -d --tmpdir=.)
+CHROOT=$(mktemp -d --tmpdir=$(pwd))
 CHROOT=$(basename ${CHROOT})
 
 RESULT_DIR=/tmp/packages
 
+CACHE_DIR=$(mktemp -d --tmpdir=$(pwd))
+
+TEMP_CHROOT=$(mktemp -d --tmpdir=$(pwd))
+TEMP_PKGBUILD=$(mktemp)
+
 # Necessary, or the chroot user created below won't be able to execute anything
 chmod -R 0755 ${CHROOT}
 
+# Make sure the chroot pacman can write to the cache dir
+chmod -R 0755 ${CACHE_DIR}
+
+### Download ###################################################################
+
+# Everything writing to /var/cache/pacman/pkg/ MUST be done here to avoid
+# threading/parallel execution issues
+# TODO: Use clean pacman configuration file
+(
+  flock 321 || (echo "Failed to acquire lock on pacman cache!" && exit 1)
+
+  # Prevent tons of useless crap from spewing out
+  set +x
+
+  # Remove any packages in the local repo from the pacman cache. The chroot
+  # should download them into its own cache.
+  for i in ${LOCALREPO}/*.pkg.tar.xz; do
+    if [ -f /var/cache/pacman/pkg/$(basename ${i}) ]; then
+      rm /var/cache/pacman/pkg/$(basename ${i})
+    fi
+  done
+
+  # Create temporary mini-chroot to store database files
+  mkdir -p ${TEMP_CHROOT}/var/lib/pacman/
+
+  echo "Downloading base packages for chroot..."
+  pacman --arch ${ARCH} --sync --refresh --downloadonly --noconfirm \
+         --root ${TEMP_CHROOT} --cachedir /var/cache/pacman/pkg/ \
+         ${CHROOT_PACKAGES[@]}
+
+  echo "Downloading dependencies and build dependencies..."
+  cat ${PACKAGE_DIR}/PKGBUILD > ${TEMP_PKGBUILD}
+  chown nobody:nobody ${TEMP_PKGBUILD}
+  depends=$(sudo -u nobody bash -c "source ${TEMP_PKGBUILD} && \
+                                    echo \${depends[@]}")
+  makedepends=$(sudo -u nobody bash -c "source ${TEMP_PKGBUILD} && \
+                                        echo \${makedepends[@]}")
+  available="$(pacman -Sl core extra community | cut -d' ' -f2)"
+  list=""
+  for i in ${depends} ${makedepends}; do
+    if echo "${available}" | tr ' ' '\n' | grep -q "^${i}$"; then
+      list+=" ${i}"
+    fi
+  done
+  pacman --arch ${ARCH} --sync --refresh --downloadonly --noconfirm \
+         --root ${TEMP_CHROOT} --cachedir /var/cache/pacman/pkg/ ${list}
+
+  # Copy /var/cache/pacman/pkg/ to the chroot-specific cache directory
+  cp /var/cache/pacman/pkg/*.pkg.tar.xz ${CACHE_DIR}/
+
+  set -x
+) 321>$(dirname ${0})/cache.lock
+
+################################################################################
+
+### Create chroot ##############################################################
+
 # Create base chroot
-setarch ${ARCH} mkarchroot -f ${CHROOT} base base-devel sudo curl
+setarch ${ARCH} mkarchroot -f -c ${CACHE_DIR} ${CHROOT} \
+                           ${CHROOT_PACKAGES[@]}
 
 # Set up /etc/makepkg.conf
 cat >> ${CHROOT}/etc/makepkg.conf << EOF
@@ -161,9 +228,12 @@ fi
 # Copy packaging
 mkdir ${CHROOT}/tmp/${PACKAGE}/
 cp -v "${PACKAGE_DIR}/PKGBUILD" ${CHROOT}/tmp/${PACKAGE}/
-install=$(bash -c "source ${PACKAGE_DIR}/PKGBUILD && echo \${install}")
-sources=$(bash -c "source ${PACKAGE_DIR}/PKGBUILD && echo \${source[@]}")
-extrafiles=$(bash -c "source ${PACKAGE_DIR}/PKGBUILD && echo \${extrafiles[@]}")
+install=$(sudo -u nobody bash -c "source ${TEMP_PKGBUILD} && \
+                                  echo \${install}")
+sources=$(sudo -u nobody bash -c "source ${TEMP_PKGBUILD} && \
+                                  echo \${source[@]}")
+extrafiles=$(sudo -u nobody bash -c "source ${TEMP_PKGBUILD} && \
+                                     echo \${extrafiles[@]}")
 if [ -f "${PACKAGE_DIR}/${install}" ]; then
   cp -v "${PACKAGE_DIR}/${install}" ${CHROOT}/tmp/${PACKAGE}/
 fi
@@ -177,14 +247,13 @@ for i in ${extrafiles}; do
 done
 
 # Create new user
-mkarchroot \
-  -r "useradd --create-home --shell /bin/bash --user-group builder" \
-  ${CHROOT}
+mkarchroot -r "useradd --create-home --shell /bin/bash --user-group builder" \
+           -c ${CACHE_DIR} ${CHROOT}
 
 # Fix permissions
 mkdir ${CHROOT}${RESULT_DIR}/
-mkarchroot -r "chown -R builder:builder ${RESULT_DIR} \
-                                        /tmp/${PACKAGE}/" ${CHROOT}
+mkarchroot -r "chown -R builder:builder ${RESULT_DIR} /tmp/${PACKAGE}/" \
+           -c ${CACHE_DIR} ${CHROOT}
 
 # Make sure the builder user can run "pacman" to install the build dependencies
 echo "builder ALL=(ALL) ALL,NOPASSWD: /usr/bin/pacman" \
@@ -194,23 +263,19 @@ echo "builder ALL=(ALL) ALL,NOPASSWD: /usr/bin/pacman" \
 mkdir -p ${LOCALREPO}/ ${CHROOT}${LOCALREPO}/
 mount --bind ${LOCALREPO}/ ${CHROOT}${LOCALREPO}/
 if [ -f ${LOCALREPO}/${REPO}.db ]; then
-  (
-    flock 321 || (echo "Failed to acquire lock on pacman cache!" && exit 1)
-    setarch ${ARCH} mkarchroot -r "pacman -Sy ${PROGRESSBAR}" ${CHROOT}
-  ) 321>${LOCALREPO}/cache.lock
+  setarch ${ARCH} mkarchroot -r "pacman -Sy ${PROGRESSBAR}" \
+                             -c ${CACHE_DIR} ${CHROOT}
 fi
 
 # Download sources and install build dependencies
-(
-  flock 321 || (echo "Failed to acquire lock on pacman cache!" && exit 1)
-  setarch ${ARCH} mkarchroot \
-    -r "
-    su - builder -c 'cd /tmp/${PACKAGE} && \
-                     makepkg --syncdeps --nobuild --nocolor --noconfirm \
-                             ${PROGRESSBAR}'
-    " \
-    ${CHROOT}
-) 321>${LOCALREPO}/cache.lock
+setarch ${ARCH} mkarchroot \
+  -r "
+  su - builder -c 'cd /tmp/${PACKAGE} && \
+                   makepkg --syncdeps --nobuild --nocolor --noconfirm \
+                           ${PROGRESSBAR}'
+  " \
+  -c ${CACHE_DIR} \
+  ${CHROOT}
 
 # Build package
 # TODO: Enable signing
@@ -220,33 +285,26 @@ setarch ${ARCH} mkarchroot \
                    makepkg --clean --check --noconfirm --nocolor --noextract \
                            ${PROGRESSBAR}'
   " \
+  -c ${CACHE_DIR} \
   ${CHROOT}
 
+################################################################################
+
+### Create or update local repo ################################################
+
 # Move out packages
-rm -f ${LOCALREPO}/*.db*
-rm -f ${LOCALREPO}/*.files*
-cp ${CHROOT}${RESULT_DIR}/* ${LOCALREPO}/
 if [ "x${KEEP_COPY}" = "xtrue" ]; then
   cp ${CHROOT}${RESULT_DIR}/* ${PACKAGE_DIR}/
 fi
 
 # Update repo. Make sure that a lock is acquired before performing the operation
+# TODO: Remove old packages
 echo "Attempting to acquire lock on local repo..."
 (
   flock 123 || (echo "Failed to acquire lock on local repo!" && exit 1)
-  echo "Acquired lock on local repo"
-  # We must clear out existing packages in the cache. Rebuilds without changing
-  # the package version and release will cause the sha256sums in ${LOCALREPO}
-  # and /var/cache/pacman/pkg/ to mismatch causing pacman to fail. It would be
-  # better if it was possible to tell pacman not to cache the local repo.
-  (
-    flock 321 || (echo "Failed to acquire lock on pacman cache!" && exit 1)
-    for i in ${LOCALREPO}/*.pkg.tar.xz; do
-      if [ -f /var/cache/pacman/pkg/$(basename ${i}) ]; then
-        rm /var/cache/pacman/pkg/$(basename ${i})
-      fi
-    done
-  ) 321>${LOCALREPO}/cache.lock
+  rm -f ${LOCALREPO}/*.db*
+  rm -f ${LOCALREPO}/*.files*
+  cp ${CHROOT}${RESULT_DIR}/* ${LOCALREPO}/
   # TODO: Enable signing
   repo-add ${LOCALREPO}/${REPO}.db.tar.xz ${LOCALREPO}/*.pkg.tar.xz
 ) 123>${LOCALREPO}/repo.lock
