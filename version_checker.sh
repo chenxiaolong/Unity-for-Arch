d0683ccef68259d2cf13dd6602f01bef84d60b79
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-03-13 16:20:17 -0400

    [version_checker.sh] Add support for native packages in a PPA

--- version_checker.sh
+++ version_checker.sh
@@ -95,10 +95,17 @@ get_ppa_version() {
     echo "No PPA was provided"
     exit 1
   fi
-  wget -q -O - \
-    "http://ppa.launchpad.net/${2/#ppa:/}/ubuntu/pool/main/${1:0:1}/${1}/" | \
-    sed -n "s/.*>${1}_\(.*\)-\(.*\)\.\(debian\|diff\)\.[a-z\.]\+<.*/\1 \2/p" | \
-    tail -n 1
+  if [ "x${3}" == "xnative" ]; then
+    wget -q -O - \
+      "http://ppa.launchpad.net/${2/#ppa:/}/ubuntu/pool/main/${1:0:1}/${1}/" | \
+      sed -n "s/.*>${1}_\(.*\)-\(.*\)\.tar\.[a-z\.]\+<.*/\1 \2/p" | \
+      tail -n 1
+  else
+    wget -q -O - \
+      "http://ppa.launchpad.net/${2/#ppa:/}/ubuntu/pool/main/${1:0:1}/${1}/" | \
+      sed -n "s/.*>${1}_\(.*\)-\(.*\)\.\(debian\|diff\)\.[a-z\.]\+<.*/\1 \2/p" | \
+      tail -n 1
+  fi
 }
 
 get_qt4_version() {
