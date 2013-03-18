e6d55d8a5022e84303240ae2b1ae18826ab64237
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-03-18 18:34:06 -0400

    [libxfixes-ubuntu] Removed: only required by Unity, which was just patched for libxi 1.7

--- libxfixes-ubuntu/compare_versions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
-echo -e "Upstream version: $(get_xorg_version libXfixes lib)"
-echo -e "Ubuntu version:   $(get_ubuntu_version libxfixes ${1:-raring})"
