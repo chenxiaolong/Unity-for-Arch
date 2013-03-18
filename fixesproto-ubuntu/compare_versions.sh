e2bf95c4ca5d5f80e81b917eef05c4f087080599
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-03-18 18:35:51 -0400

    [fixesproto-ubuntu] Removed: only needed by libxfixes-ubuntu, which was just removed

--- fixesproto-ubuntu/compare_versions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "PKGBUILD version: ${pkgver%.*} ${_ubuntu_rel}"
-echo -e "Upstream version: $(get_xorg_version fixesproto proto)"
-echo -e "Ubuntu version:   $(get_ubuntu_version x11proto-fixes ${1:-raring})"
