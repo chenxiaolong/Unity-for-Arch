3219cd78ae7c26cb14d666a2df556ee12767da9a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-03-18 18:30:42 -0400

    [xorg-server-ubuntu] Outdated and Unity is now patched for libxi 1.7

--- xorg-server-ubuntu/compare_versions.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "PKGBUILD version:   ${pkgver}"
-echo -e "Upstream version:   $(get_xorg_version ${pkgname%-*} xserver)"
-echo -e "Arch Linux version: $(get_archlinux_version ${pkgname%-*} extra x86_64)"
-echo -e "Ubuntu version:     $(get_ubuntu_version ${pkgname%-*} ${1:-raring})"
