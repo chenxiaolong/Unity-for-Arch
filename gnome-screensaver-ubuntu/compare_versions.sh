2a4ec1fa9d2b28687e88d790838f3c797f334f4c
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-03-26 01:38:25 -0400

    [gnome-screensaver-ubuntu] Add version comparison script

---
+++ gnome-screensaver-ubuntu/compare_versions.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+source "$(dirname ${0})/../version_checker.sh"
+
+echo -e "PKGBUILD version:   ${pkgver} ${_ubuntu_rel}"
+echo -e "Upstream version:   $(get_gnome_version ${pkgname%-*} 3.6)"
+echo -e "Arch Linux version: $(get_archlinux_version ${pkgname%-*} extra x86_64)"
+echo -e "Ubuntu version:     $(get_ubuntu_version ${pkgname%-*} ${1:-raring})"
