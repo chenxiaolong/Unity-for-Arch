4826c231d28c0d6724ed2ef6d3fc5a32a0b727e3
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-03-27 19:21:28 -0400

    [gnome-settings-daemon-ubuntu] Update to version 3.8.0 with PPA version 3.7.91 and release 0ubuntu1~raring1

--- gnome-settings-daemon-ubuntu/compare_versions.sh
+++ gnome-settings-daemon-ubuntu/compare_versions.sh
@@ -3,7 +3,8 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version:   ${_actual_ver} ${_ubuntu_rel}"
-echo -e "Upstream version:   $(get_gnome_version ${pkgname%-*} 3.6)"
+echo -e "PKGBUILD version:   ${pkgver} PPA ${_ppa_ver} ${_ppa_rel}"
+echo -e "Upstream version:   $(get_gnome_version ${pkgname%-*} 3.8)"
 echo -e "Arch Linux version: $(get_archlinux_version ${pkgname%-*} extra x86_64)"
 echo -e "Ubuntu version:     $(get_ubuntu_version ${pkgname%-*} ${1:-raring})"
+echo -e "PPA version:        $(get_ppa_version ${pkgname%-*} ppa:gnome3-team/gnome3-staging)"
