7f60443eb8356b862e8892d658b37d9c96f4986f
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-03-27 20:15:34 -0400

    [gtk3-ubuntu] Update version comparison script

--- gtk3-ubuntu/compare_versions.sh
+++ gtk3-ubuntu/compare_versions.sh
@@ -4,7 +4,7 @@ source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version:   ${pkgver} PPA ${_ppa_ver} ${_ppa_rel}"
-echo -e "Upstream version:   $(get_gnome_version gtk+ 3.6)"
+echo -e "Upstream version:   $(get_gnome_version gtk+ 3.8)"
 echo -e "Arch Linux version: $(get_archlinux_version gtk3 extra x86_64)"
 echo -e "Ubuntu version:     $(get_ubuntu_version gtk+3.0 ${1:-raring})"
 echo -e "PPA version:        $(get_ppa_version gtk+3.0 ppa:gnome3-team/gnome3)"
