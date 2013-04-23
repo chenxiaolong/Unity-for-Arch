49061a5cd81dc229dc7546db7fc6d04d36a26c5c
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-04-22 21:43:45 -0400

    [credentials-preferences-ubuntu] Removed: part of gnome-control-center-ubuntu now

--- credentials-preferences-ubuntu/compare_versions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "PKGBUILD version:   ${pkgver}"
-echo -e "Arch Linux version: $(get_archlinux_version ${pkgname%-*} community x86_64)"
-echo -e "Translations:       ${_translations}"
