31e2780787b8dfca60cda4d9db66e08f85a780ac
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-20 15:23:52 -0400

    [libtimezonemap] Moved from Unity-for-Arch-LiveCD

---
+++ libtimezonemap/compare_versions.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+source "$(dirname ${0})/../version_checker.sh"
+
+echo -e "PKGBUILD version: ${pkgver}"
+echo -e "Upstream version: (none)"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-saucy} native)"
