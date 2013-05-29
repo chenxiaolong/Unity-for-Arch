f4266e06c9d48d30c5b1a8ba9dc3fa771e9ac5ab
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-05-29 13:31:41 -0400

    [gnome-control-center-unity] Update to version 1.3daily13.05.20ubuntu.unity.next

--- gnome-control-center-unity/compare_versions.sh
+++ gnome-control-center-unity/compare_versions.sh
@@ -5,5 +5,5 @@ source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver}"
 echo -e "Upstream version: (none)"
-echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-raring})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-saucy})"
 echo -e "Translations:     ${_translations}"
