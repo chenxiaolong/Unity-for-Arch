db1d2667df7c87dfb202fadb5e40b7da5c6ac4b8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-06-06 19:29:54 -0400

    [gtk3-ubuntu] Update to version 3.8.2 with Ubuntu release 0ubuntu3

--- gtk3-ubuntu/compare_versions.sh
+++ gtk3-ubuntu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version:   ${pkgver} ${_ppa_rel}"
+echo -e "PKGBUILD version:   ${pkgver} ${_ubuntu_rel}"
 echo -e "Upstream version:   $(get_gnome_version gtk+ 3.8)"
 echo -e "Arch Linux version: $(get_archlinux_version gtk3 extra x86_64)"
 echo -e "Ubuntu version:     $(get_ubuntu_version gtk+3.0 ${1:-saucy})"
