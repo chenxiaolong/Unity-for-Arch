9d9bed18f8a043a1f9eccbc93f1d08e180048e85
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-06-07 00:45:37 -0400

    [gsettings-desktop-schemas-ubuntu] Switch to Ubuntu 13.10 releases instead of PPA releases

--- gsettings-desktop-schemas-ubuntu/compare_versions.sh
+++ gsettings-desktop-schemas-ubuntu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version:   ${pkgver} PPA ${_ppa_ver} ${_ppa_rel}"
+echo -e "PKGBUILD version:   ${pkgver} Ubuntu ${_ubuntu_ver} ${_ubuntu_rel}"
 echo -e "Upstream version:   $(get_gnome_version ${pkgname%-*} 3.8)"
 echo -e "Arch Linux version: $(get_archlinux_version ${pkgname%-*} extra x86_64)"
 echo -e "Ubuntu version:     $(get_ubuntu_version ${pkgname%-*} ${1:-saucy})"
