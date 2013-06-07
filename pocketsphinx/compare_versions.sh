c2e91573586fe238d169ff0add13ef561f25d60c
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-06-07 15:17:13 -0400

    [sphinxbase] Add version comparison script

---
+++ pocketsphinx/compare_versions.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+source "$(dirname ${0})/PKGBUILD"
+source "$(dirname ${0})/../version_checker.sh"
+
+echo -e "PKGBUILD version: ${pkgver} ${_ubuntu_rel}"
+UPSTREAM_VER=$(wget -q -O - 'http://sourceforge.net/projects/cmusphinx/files/pocketsphinx/' | sed -n 's/^.*a href="\/projects\/cmusphinx\/files\/pocketsphinx\/\(.*\)\/".*$/\1/p' | head -n 1)
+echo -e "Upstream version: ${UPSTREAM_VER}"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-saucy})"
