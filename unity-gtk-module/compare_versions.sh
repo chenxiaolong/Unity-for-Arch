51afb5d2c41d50c8ead63b7c7e87f260e839e22a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-06-09 16:55:59 -0400

    [unity-gtk-module] Added: appmenu-gtk is obsolete

--- unity-gtk-module/compare_versions.sh
+++ unity-gtk-module/compare_versions.sh
@@ -4,5 +4,5 @@ source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver}"
-echo -e "Upstream version: $(get_launchpad_version ${pkgbase})"
-echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgbase} ${1:-raring})"
+echo -e "Upstream version: (none)"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-saucy})"
