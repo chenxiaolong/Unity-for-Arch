67050461076200d5a57b4206ae09adf57f621c1f
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-06-14 15:06:04 -0400

    [unity-scopes] Combine all scopes into one package

--- unity-scopes/compare_versions.sh
+++ unity-scopes/compare_versions.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source PKGBUILD
+
 echo "Downloading Ubuntu 13.10 Source Package Database..."
 [ -f Sources.bz2 ] && rm Sources.bz2
 [ -f Sources ] && rm Sources
@@ -21,26 +23,14 @@ LINE_THIN=$(printline '-')
 
 for i in ${PACKAGES[@]}; do
   UBUNTU_VER=$(grep -A2 "Package: unity-scope-${i}$" Sources | sed -n 's/^Version: \(.*\)/\1/p')
-  PKGBUILD_VER=$(python3 <<EOF
-import yaml
-import sys
-fd = open('$(dirname ${0})/pkginfo.yaml', 'r')
-data = yaml.load(fd, yaml.CLoader)
-for i in data['packages']:
-  if "${i}" == i['name']:
-    print(i['version'] + '-' + i['release'])
-    fd.close()
-    sys.exit(0)
-print("NOTFOUND")
-fd.close()
-EOF
-)
+
+  PKGBUILD_VER=$(eval "echo \${_ver_${i}} \${_rel_${i}}")
 
   echo "${LINE_THICK}"
   echo "Package: unity-scope-${i}"
   echo "${LINE_THIN}"
-  echo "spec file version: ${PKGBUILD_VER}"
-  echo "Ubuntu version:    ${UBUNTU_VER}"
+  echo "PKGBUILD version: ${PKGBUILD_VER}"
+  echo "Ubuntu version:   ${UBUNTU_VER}"
 done
 
 rm Sources
