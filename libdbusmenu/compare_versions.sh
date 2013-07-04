0b1eebe84624e7cecf01729496d1678e90961776
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-07-03 20:18:53 -0400

    [libdbusmenu] Update to version 12.10.3+13.10.20130628

--- libdbusmenu/compare_versions.sh
+++ libdbusmenu/compare_versions.sh
@@ -3,6 +3,6 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version: ${pkgver}"
+echo -e "PKGBUILD version: ${_realver}"
 echo -e "Upstream version: $(get_launchpad_version dbusmenu libdbusmenu)"
 echo -e "Ubuntu version:   $(get_ubuntu_version libdbusmenu ${1:-saucy})"
