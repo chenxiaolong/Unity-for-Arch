33a470ed55ace882b56cdc9e8ac0d60106cc97ee
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-07-03 21:59:01 -0400

    [nautilus-ubuntu] Update to version 3.8.2 with Ubuntu release 0ubuntu1

--- nautilus-ubuntu/compare_versions.sh
+++ nautilus-ubuntu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version:   ${pkgver} ${_ppa_rel}"
+echo -e "PKGBUILD version:   ${pkgver} ${_ubuntu_rel}"
 echo -e "Upstream version:   $(get_gnome_version ${pkgbase%-*} 3.8)"
 echo -e "Arch Linux version: $(get_archlinux_version ${pkgbase%-*} extra x86_64)"
 echo -e "Ubuntu version:     $(get_ubuntu_version ${pkgbase%-*} ${1:-raring})"
