c72c3f91def3f30762970dbe09aeb27f27c04745
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-08-15 01:49:44 -0400

    [compiz-ubuntu] Track saucy package versions

--- compiz-ubuntu/compare_versions.sh
+++ compiz-ubuntu/compare_versions.sh
@@ -5,4 +5,4 @@ source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver} ${_ubuntu_rel}"
 echo -e "Upstream version: $(get_launchpad_version compiz)"
-echo -e "Ubuntu version:   $(get_ubuntu_version compiz ${1:-raring})"
+echo -e "Ubuntu version:   $(get_ubuntu_version compiz ${1:-saucy})"
