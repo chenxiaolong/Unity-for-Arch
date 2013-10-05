dada6097577d579a7a799245cbbaea0a3af5f518
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-10-05 17:47:36 -0400

    [gtk2-ubuntu] Update to version 2.24.21

--- gtk2-ubuntu/compare_versions.sh
+++ gtk2-ubuntu/compare_versions.sh
@@ -3,7 +3,7 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version:   ${pkgver} ${_ubuntu_rel}"
+echo -e "PKGBUILD version:   ${pkgver} Ubuntu ${_ubuntu_ver} ${_ubuntu_rel}"
 echo -e "Upstream version:   $(get_gnome_version gtk+ 2.24)"
 echo -e "Arch Linux version: $(get_archlinux_version gtk2 extra x86_64)"
 echo -e "Ubuntu version:     $(get_ubuntu_version gtk+2.0 ${1:-saucy})"
