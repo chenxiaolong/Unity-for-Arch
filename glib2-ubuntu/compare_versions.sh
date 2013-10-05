625e0f2a7465001df33dc12994d1bf7791bb8e66
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-10-05 17:40:18 -0400

    [glib2-ubuntu] Removed

--- glib2-ubuntu/compare_versions.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "PKGBUILD version:   ${pkgver}"
-echo -e "Upstream version:   $(get_gnome_version glib 2.37)"
-echo -e "Arch Linux version: $(get_archlinux_version glib2 core x86_64)"
-echo -e "Ubuntu version:     $(get_ubuntu_version glib2.0 ${1:-saucy})"
