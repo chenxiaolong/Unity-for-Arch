3231acba4700fc9de3c428ebbc68fbe2570eaf19
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-10-05 20:23:09 -0400

    [gnome-session-ubuntu] Update to version 3.10.0 with PPA release 0ubuntu1~saucy1

--- gnome-session-ubuntu/compare_versions.sh
+++ gnome-session-ubuntu/compare_versions.sh
@@ -3,9 +3,9 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version:   ${pkgver} ${_ubuntu_rel}"
-echo -e "Upstream version:   $(get_gnome_version ${pkgname%-*} 3.8)"
+echo -e "PKGBUILD version:   ${pkgver} ${_ppa_rel}"
+echo -e "Upstream version:   $(get_gnome_version ${pkgname%-*} 3.10)"
 echo -e "Arch Linux version: $(get_archlinux_version ${pkgname%-*} extra x86_64)"
 echo -e "Ubuntu version:     $(get_ubuntu_version ${pkgname%-*} ${1:-saucy})"
-echo -e "PPA version:        $(get_ppa_version ${pkgname%-*} ppa:gnome3-team/gnome3)"
+echo -e "PPA version:        $(get_ppa_version ${pkgname%-*} ppa:gnome3-team/gnome3-staging)"
 echo -e "Translations:       ${_translations}"
