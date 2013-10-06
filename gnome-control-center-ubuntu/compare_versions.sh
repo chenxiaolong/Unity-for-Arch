363d8c569c4e23278b4197126f98e4163644bbc3
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-10-05 20:37:20 -0400

    [gnome-control-center-ubuntu] Update to version 3.10.0 with PPA release 0ubuntu1~saucy2

--- gnome-control-center-ubuntu/compare_versions.sh
+++ gnome-control-center-ubuntu/compare_versions.sh
@@ -4,7 +4,7 @@ source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version:   ${pkgver} ${_ppa_rel}"
-echo -e "Upstream version:   $(get_gnome_version ${pkgname%-*} 3.8)"
+echo -e "Upstream version:   $(get_gnome_version ${pkgname%-*} 3.10)"
 echo -e "Arch Linux version: $(get_archlinux_version ${pkgname%-*} extra x86_64)"
 echo -e "Ubuntu version:     $(get_ubuntu_version ${pkgname%-*} ${1:-saucy})"
 echo -e "PPA version:        $(get_ppa_version ${pkgname%-*} ppa:gnome3-team/gnome3-staging)"
