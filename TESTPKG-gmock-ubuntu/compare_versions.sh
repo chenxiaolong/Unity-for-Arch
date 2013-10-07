fb1339c1613ead2911151783145a6b8ecab61bb9
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-10-07 15:23:00 -0400

    [TESTPKG-*] Removed: tired of chasing down broken tests and skipping them

--- TESTPKG-gmock-ubuntu/compare_versions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver} ${_ubuntu_rel}"
-echo -e "Upstream version: $(get_googlecode_version googlemock gmock)"
-echo -e "Ubuntu version:   $(get_ubuntu_version google-mock ${1:-saucy})"
