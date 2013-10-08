afce5350bfa6ef0cc5602ff4be55327b48468ee8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-10-08 17:28:30 -0400

    [credentials-preferences-ubuntu] Removed: Not compatible with GNOME 3.10

--- credentials-preferences-ubuntu/compare_versions.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "PKGBUILD version:   ${_actual_ver}${_extra_ver}"
-echo -e "Arch Linux version: $(get_archlinux_version ${pkgname%-*} community x86_64)"
-echo -e "Ubuntu version:     $(get_ubuntu_version gnome-control-center-signon ${1:-saucy})"
-echo -e "Translations:       ${_translations}"
