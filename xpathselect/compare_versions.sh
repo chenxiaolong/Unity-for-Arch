b72a6d9f76a6034df69c5e421ab01adb514dfa81
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-12-10 22:40:32 -0500

    [xpathselect] Update to version 1.4+14.04.20131106.1

--- xpathselect/compare_versions.sh
+++ xpathselect/compare_versions.sh
@@ -5,4 +5,4 @@ source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver}"
 echo -e "Upstream version: (none)"
-echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-saucy})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-trusty})"
