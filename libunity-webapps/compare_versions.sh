626aefc56f2746a558279f05b2902c4f03005413
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-12-10 22:16:37 -0500

    [libunity-webapps] Update to version 2.5.0~+14.04.20131108.2

--- libunity-webapps/compare_versions.sh
+++ libunity-webapps/compare_versions.sh
@@ -5,5 +5,5 @@ source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver}"
 echo -e "Upstream version: $(get_launchpad_version ${pkgname} unity_webapps)"
-echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-saucy})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-trusty})"
 echo -e "Translations:     ${_translations}"
