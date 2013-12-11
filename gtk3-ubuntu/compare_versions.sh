46af8cf638fcd76654765113676c2f92d8a97a9a
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-12-10 19:13:33 -0500

    [gtk3-ubuntu] Update to version 3.10.6 with Ubuntu release 0ubuntu1

--- gtk3-ubuntu/compare_versions.sh
+++ gtk3-ubuntu/compare_versions.sh
@@ -3,8 +3,8 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version:   ${pkgver} ${_ppa_rel}"
+echo -e "PKGBUILD version:   ${pkgver} ${_ubuntu_rel}"
 echo -e "Upstream version:   $(get_gnome_version gtk+ 3.10)"
 echo -e "Arch Linux version: $(get_archlinux_version gtk3 extra x86_64)"
-echo -e "Ubuntu version:     $(get_ubuntu_version gtk+3.0 ${1:-saucy})"
+echo -e "Ubuntu version:     $(get_ubuntu_version gtk+3.0 ${1:-trusty})"
 echo -e "PPA version:        $(get_ppa_version gtk+3.0 ppa:gnome3-team/gnome3-next)"
