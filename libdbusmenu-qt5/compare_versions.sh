c69f24d8de3e6cc607cf7751a5829c4b1b1c2b8e
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-03-12 18:27:52 -0400

    [libdbusmenu-qt5] Override AUR version

--- libdbusmenu-qt5/compare_versions.sh
+++ libdbusmenu-qt5/compare_versions.sh
@@ -4,5 +4,5 @@ source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${_actual_ver}${_extra_ver}"
-echo -e "Upstream version: $(get_launchpad_version ${pkgname})"
-echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname} ${1:-trusty})"
+echo -e "Upstream version: $(get_launchpad_version ${pkgname%5})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname%5} ${1:-trusty})"
