8ee64e303c9c87c947867dd2f6cae589cb4ad0b2
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-03-12 18:33:30 -0400

    [zeitgeist-ubuntu] Track trusty repo

--- zeitgeist-ubuntu/compare_versions.sh
+++ zeitgeist-ubuntu/compare_versions.sh
@@ -5,4 +5,4 @@ source "$(dirname ${0})/../version_checker.sh"
 
 echo -e "PKGBUILD version: ${pkgver} ${_ubuntu_rel}"
 echo -e "Upstream version: $(get_launchpad_version ${pkgname%-*})"
-echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname%-*} ${1:-saucy})"
+echo -e "Ubuntu version:   $(get_ubuntu_version ${pkgname%-*} ${1:-trusty})"
