a1c95be262ec275f80975edd4b6d9a153c31048e
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-03-12 22:16:32 -0400

    [qt4-ubuntu] Fix version comparison script

--- qt4-ubuntu/compare_versions.sh
+++ qt4-ubuntu/compare_versions.sh
@@ -3,6 +3,6 @@
 IGNORE_NO_QTWEBKIT=yes source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version: ${pkgver} Ubuntu ${_ubuntu}"
-echo -e "Upstream version: $(get_qt4_version)"
-echo -e "Ubuntu version:   $(get_ubuntu_version qt4-x11 ${1:-trusty})"
+echo -e "PKGBUILD version:   ${pkgver} Ubuntu ${_ubuntu}"
+echo -e "Arch Linux version: $(get_archlinux_version ${pkgname%-*} extra x86_64)"
+echo -e "Ubuntu version:     $(get_ubuntu_version qt4-x11 ${1:-trusty})"
