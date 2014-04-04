48a8a4543d48297eb092a6e4f1d5758f13b50ef5
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-04-04 15:55:43 -0400

    [nautilus-ubuntu] Update to version 3.12.0 with PPA release 0ubuntu1~trusty1

--- nautilus-ubuntu/compare_versions.sh
+++ nautilus-ubuntu/compare_versions.sh
@@ -3,8 +3,8 @@
 source "$(dirname ${0})/PKGBUILD"
 source "$(dirname ${0})/../version_checker.sh"
 
-echo -e "PKGBUILD version:   ${pkgver} ${_ubuntu_rel}"
-echo -e "Upstream version:   $(get_gnome_version ${pkgbase%-*} 3.10)"
+echo -e "PKGBUILD version:   ${pkgver} ${_ppa_rel}"
+echo -e "Upstream version:   $(get_gnome_version ${pkgbase%-*} 3.12)"
 echo -e "Arch Linux version: $(get_archlinux_version ${pkgbase%-*} extra x86_64)"
 echo -e "Ubuntu version:     $(get_ubuntu_version ${pkgbase%-*} ${1:-trusty})"
 echo -e "PPA version:        $(get_ppa_version ${pkgbase%-*} ppa:gnome3-team/gnome3-staging)"
