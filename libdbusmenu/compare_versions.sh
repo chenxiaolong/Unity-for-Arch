121844842796af0a4877b04113ba9a0405946e4c
Christopher Reimer <mail@creimer.net>
2014-04-22 14:07:12 +0200

    Update repo-make.conf and fix some dependency problems

--- libdbusmenu/compare_versions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(dirname ${0})/PKGBUILD"
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "PKGBUILD version: ${_realver}"
-echo -e "Upstream version: $(get_launchpad_version dbusmenu libdbusmenu)"
-echo -e "Ubuntu version:   $(get_ubuntu_version libdbusmenu ${1:-trusty})"
