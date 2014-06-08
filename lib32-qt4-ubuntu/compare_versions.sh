396c0dcdecadd50ace755aac46ccbc924fed96d8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-06-08 12:57:00 -0400

    Remove extra file

--- lib32-qt4-ubuntu/compare_versions.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-IGNORE_NO_QTWEBKIT=yes source "$(dirname ${0})/PKGBUILD"
-source "$(dirname ${0})/../version_checker.sh"
-
-echo -e "PKGBUILD version:   ${pkgver} Ubuntu ${_ubuntu}"
-echo -e "Arch Linux version: $(get_archlinux_version ${pkgname%-*} multilib x86_64)"
-echo -e "Ubuntu version:     $(get_ubuntu_version qt4-x11 ${1:-utopic})"
